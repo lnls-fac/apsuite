89d8831dd8cf9b0b069b80947f3a09c9c50c9c43
fernando <fernandohds564@gmail.com>
2015-09-29 22:00:51 -0300

    Creation of jupyter notebook to test lattice_errors functions and finished implementation of lattice_errors.generate_erros function.

--- lattice_errors/lattice_errors.py
+++ lattice_errors/lattice_errors.py
@@ -1,97 +1,119 @@
 
 import numpy as _np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as _plt
 import scipy.stats as _scystat
 
 import pyaccel as _pyaccel
 import mathphys as _mp
 
-def generate_errors(name,accel,config,fam_data,nr_mach=20,cutoff=1,rndtype='gauss'):
+_ERRORTYPES = ('x','y','roll','yaw','pitch','excit','k_dip')
+
+def generate_errors(name,acc,config,fam_data=None,nr_mach=20,cutoff=1,rndtype='gauss'):
     """Generates random errors to be applied in the model by the function apply_errors.
 
- INPUTS:
-   name       : a string to be used as name to save the configuration data
-   accel      : is the accelerator model.
-   config     : dictionary with configuration of the errors with keys
-                'mags' and/or 'girders'. All of them are optional:
-     'mags'   : dictionary with arbitrary named keys whose values are
-                dictionaries with keys:
-       'labels': list of family names
-       'sigma' : dictionary. possible keys:'x','y','roll','excit','k_dip','yaw','pitch'.
-                 Values are errors definition (1-sigma for gauss, max for uniform)
+    INPUTS:
+      name       : a string to be used as name to save the configuration data
+      acc        : is the accelerator model.
+      config     : dictionary with configuration of the errors with keys
+                   'mags' and/or 'girders'. All of them are optional:
+        'mags'   : dictionary with arbitrary named keys whose values are
+                   dictionaries with keys:
+          'labels': list of family names
+          'sigma' : dictionary. possible keys:'x','y','roll','excit','k_dip','yaw','pitch'.
+                    Values are errors definition (1-sigma for gauss, max for uniform)
 
-     'girder' : dictionary with definition of girder errors. Possible keys:
-                'x','y','roll','excit','k_dip','yaw','pitch'.
-                Values are errors definition (1-sigma for gauss, max for uniform)
-   fam_data : dictionary with families data created by function
-              accelerator.get_family_data().
-   nr_mach : generate errors for this number of machines.
-   cutoff  : number of sigmas to cutoff the distribution (default is infinity)
-   rndtype : type of distribution. Possible values: 'sin' and 'gauss' (default is 'gauss')
+        'girder' : dictionary with definition of girder errors. Possible keys:
+                   'x','y','roll','excit','k_dip','yaw','pitch'.
+                   Values are errors definition (1-sigma for gauss, max for uniform)
+      fam_data : dictionary whose keys are family names of, at least, all magnets
+                 defined in config['mags']['labels'] and the key 'girder' and
+                 values are dictionaries with at least one key: 'index',
+                 whose value is a list of indices the magnets in the lattice.
+                 If this list is a nested list, then each sub-list is understood
+                 as segments of the same physical and the same errors will be
+                 applied to them.
+                 Its default is None, which means each instance of the magnets in
+                 the lattice will be considered as independent, with its own error
+                 and girder errors will be ignored.
+                 For sirius, this dictionary can be created with the function
+                 sirius.<'version'>.get_family_data(acc).
+      nr_mach : generate errors for this number of machines.
+      cutoff  : number of sigmas to truncate the distribution (default is infinity)
+      rndtype : type of distribution. Possible values: 'uniform' and 'gauss'.
+                Default is 'gauss'.
 
- OUTPUT:
-    errors : dictionary with keys: 'x','y','roll','yaw','pitch','excit','k_dip'.
-      Each key is a list with dimension nr_mach x len(acc)
-      with errors generated for the elements defined by the inputs. If an
-      element errors has contributions from 'mags' and 'girder', the value
-      present in this output will be the sum of them.
+    OUTPUT:
+        errors : dictionary with keys: 'x','y','roll','yaw','pitch','excit','k_dip'.
+          Each key is a list with dimension nr_mach x len(acc)
+          with errors generated for the elements defined by the inputs. If an
+          element errors has contributions from 'mags' and 'girder', the value
+          present in this output will be the sum of them.
 
- EXAMPLES:
- >>> acc = sirius.si.SI_V10.create_accelerator()
- >>> um, mrad, percent = 1e-6, 1e-3, 1e-2
- >>> config = dict({'mags':dict(),'girder':dict()})
- >>> config['mags']['quads'] = dict({'labels':list(),'sigma':dict()})
- >>> config['mags']['quads']['labels'] += ['qfa','qdb2','qfb']
- >>> config['mags']['quads']['sigma']['x']     = 40 * um * 1
- >>> config['mags']['quads']['sigma']['y']     = 40 * um * 1
- >>> config['mags']['quads']['sigma']['roll']  = 0.20 * mrad * 1
- >>> config['mags']['quads']['sigma']['excit'] = 0.05 * percent * 1
- >>> config['mags']['dips'] = dict({'labels':list(),'sigma':dict()})
- >>> config['mags']['dips']['labels'] += ['b1','b2']
- >>> config['mags']['dips']['sigma']['x']     = 40 * um * 1
- >>> config['mags']['dips']['sigma']['y']     = 40 * um * 1
- >>> config['girder']['x']     = 100 * um * 1
- >>> config['girder']['y']     = 100 * um * 1;
- >>> config['girder']['roll']  =0.20 * mrad * 1;
- >>> config['girder']['yaw']   =  20 * mrad * 0;
- >>> config['girder']['pitch'] =  20 * mrad * 0;
- >>> fam_data = sirius.si.SI_V10.get_family_data(acc)
- >>> errors = generate_errors('test',acc,config,fam_data,nr_mach=20,cutoff=2)
-"""
+    EXAMPLES:
+     >>> acc = sirius.si.SI_V10.create_accelerator()
+     >>> fam_data = sirius.SI_V10.get_family_data(acc)
+     >>> um, mrad, percent = 1e-6, 1e-3, 1e-2
+     >>> config = dict({'mags':dict(),'girder':dict()})
+     >>> config['mags']['quads'] = dict({'labels':list(),'sigma':dict()})
+     >>> config['mags']['quads']['labels'] += ['qfa','qdb2','qfb']
+     >>> config['mags']['quads']['sigma']['x']     = 40 * um * 1
+     >>> config['mags']['quads']['sigma']['y']     = 40 * um * 1
+     >>> config['mags']['quads']['sigma']['roll']  = 0.20 * mrad * 1
+     >>> config['mags']['quads']['sigma']['excit'] = 0.05 * percent * 1
+     >>> config['mags']['dips'] = dict({'labels':list(),'sigma':dict()})
+     >>> config['mags']['dips']['labels'] += ['b1','b2']
+     >>> config['mags']['dips']['sigma']['x']     = 40 * um * 1
+     >>> config['mags']['dips']['sigma']['y']     = 40 * um * 1
+     >>> config['girder']['x']     = 100 * um * 1
+     >>> config['girder']['y']     = 100 * um * 1
+     >>> config['girder']['roll']  =0.20 * mrad * 1
+     >>> config['girder']['yaw']   =  20 * mrad * 0
+     >>> config['girder']['pitch'] =  20 * mrad * 0
+     >>> errors = generate_errors('test',acc,config,fam_data,nr_mach=20,cutoff=2)
+    """
 
-    #define the random numbers generator function
+    #define the random numbers generator
     if rndtype.lower().startswith('gauss'):
         random_numbers = _scystat.truncnorm(-cutoff,cutoff).rvs
-    else:
+    elif rndtype.lower().startswith('unif'):
         random_numbers = _scystat.uniform(loc=-1,scale=2).rvs
+    else:
+        raise TypeError('Distribution type not recognized.')
 
+    #generate empty arrays to store errors
     errors = dict()
-    for errtype in ['x','y','roll','yaw','pitch','excit','k_dip']:
+    for errtype in _ERRORTYPES:
         errors[errtype] = _np.zeros((nr_mach, len(acc)))
 
 
-    _mp.save_pickle(name+'_generate_errors_input',
-                    config=config,nr_mach=nr_mach,cutoff=cutoff,rndtype=rndtype)
+    # _mp.utils.save_pickle(name+'_generate_errors_input',
+    #           config=config,nr_mach=nr_mach,cutoff=cutoff,rndtype=rndtype)
 
     if 'mags' in config:
-        for mtype in config['mags'].keys():
+        for mtype in config['mags']:
             for errtype in config['mags'][mtype]['sigma']:
                 for fam_name in config['mags'][mtype]['labels']:
-                    idx = _np.array(fam_data[fam_name]['index']).T
-                    rnd = random_numbers((nr_mach,idx.shape[1])).repeat(idx.shape[0],axis=1)
+                    if fam_data is not None:
+                        idx = fam_data[fam_name]['index']
+                    else:
+                        idx = _pyaccel.lattice.find_indices(acc,'fam_name',fam_name)
+                    idx = _np.array(idx)
+                    rnd = random_numbers((nr_mach,len(idx)))
+                    if isinstance(idx[0],(list,tuple,_np.ndarray)):
+                        rnd = rnd.repeat(len(idx[0]),axis=1)
                     errors[errtype][:,idx.ravel()] += rnd * config['mags'][mtype]['sigma'][errtype]
 
-    if 'girder' in config:
+    if ('girder' in config) and (fam_data is not None):
         for errtype in config['girder']:
             for gir_name in fam_data['girder']:
-                idx = _np.array(fam_data['girder'][gir_name]).T
-                rnd = random_numbers((nr_mach,idx.shape[1])).repeat(idx.shape[0],axis=1)
+                idx = _np.array(fam_data['girder'][gir_name]['index'])
+                rnd = random_numbers((nr_mach,1)).repeat(len(idx),axis=1)
                 errors[errtype][:,idx.ravel()] += rnd * config['girder'][errtype]
 
     return errors
 
 def apply_erros(name, machine, errors, increment=1.0):
-    """ Apply the errors generated by generate_errors to the ring model.
+    """Apply the errors generated by generate_errors to the ring model.
 
  INPUTS:
    name     : name of the file to save input parameters
@@ -121,10 +143,7 @@ def apply_erros(name, machine, errors, increment=1.0):
                 ' machine lengths.\n Using minimum of both.')
         nr_mach = min([len(machine),nr_mach])
 
-    return None
-    dim = get_dim(machine{1});
-
-    ids_idx  = [i for in range(len(machine[0]))
+    ids_idx  = [i for i in range(len(machine[0]))
                   if machine[0][i].pass_method.startswith('kicktable_pass')]
     sext_idx = [i for i in range(len(machine[0]))
                   if machine[0][i].polynom_b[2] != 0.0]
@@ -140,7 +159,7 @@ def apply_erros(name, machine, errors, increment=1.0):
         ring = machine[i][:]
         _pyccel.lattice.set_attribute(ring, 'polynom_b',sext_idx, 0.0, 1, 3)
         ring = turn_ids_off(ring, ids_idx)
-        codx, cody = calc_cod(ring, dim)
+        codx, cody = _calc_cod(ring, dim)
         x_max_all, x_rms_all = get_max_rms(codx,1e3)
         y_max_all, y_rms_all = get_max_rms(cody,1e3)
         print('{0:03d}| {1:5.2f}   {2:5.2f} | {3:5.2f}   {4:5.2f} |'.format(
@@ -148,7 +167,7 @@ def apply_erros(name, machine, errors, increment=1.0):
     print(36*'-')
 
 
-    def apply_errors_one_machine(the_ring, errors, ii, fraction):
+    def apply_errors_one_machine(ring, errors, ii, fraction):
         funs={'x':_pyaccel.lattice.add_error_misalignment_x,
               'y':_pyaccel.lattice.add_error_misalignment_y,
               'roll':_pyaccel.lattice.add_error_rotation_roll,
@@ -157,8 +176,7 @@ def apply_erros(name, machine, errors, increment=1.0):
               'excit':_pyaccel.lattice.add_error_excitation_main,
               'k_dip':_pyaccel.lattice.add_error_excitation_kdip}
 
-        for errtype in ['x','y','roll','yaw','pitch','excit','k_dip']:
-            errors[errtype] = _np.zeros((nr_mach, len(acc)))
+        for errtype in errors:
             err = fraction * errors[errtype][ii,:]
             idx = err.nonzero()
-            funs[errtype](the_ring, idx, err[idx])
+            funs[errtype](ring, idx, err[idx])
