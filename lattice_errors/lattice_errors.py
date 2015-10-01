53e0e645062c136c64603def70875962df2d98cc
fernando <fernandohds564@gmail.com>
2015-10-01 02:03:54 -0300

    start of implementation of response matrix calculation.

--- lattice_errors/lattice_errors.py
+++ lattice_errors/lattice_errors.py
@@ -30,7 +30,7 @@ def generate_errors(name,acc,config,fam_data=None,nr_mach=20,cutoff=1,rndtype='g
                  values are dictionaries with at least one key: 'index',
                  whose value is a list of indices the magnets in the lattice.
                  If this list is a nested list, then each sub-list is understood
-                 as segments of the same physical and the same errors will be
+                 as segments of the same physical magnet and the same error will be
                  applied to them.
                  Its default is None, which means each instance of the magnets in
                  the lattice will be considered as independent, with its own error
@@ -38,7 +38,7 @@ def generate_errors(name,acc,config,fam_data=None,nr_mach=20,cutoff=1,rndtype='g
                  For sirius, this dictionary can be created with the function
                  sirius.<'version'>.get_family_data(acc).
       nr_mach : generate errors for this number of machines.
-      cutoff  : number of sigmas to truncate the distribution (default is infinity)
+      cutoff  : number of sigmas to truncate the distribution (default is 1)
       rndtype : type of distribution. Possible values: 'uniform' and 'gauss'.
                 Default is 'gauss'.
 
@@ -187,3 +187,123 @@ def _calc_cod(ring):
     else:
         orb = _pyaccel.tracking.findorbit4(ring,indices='open')
     return orb[0], orb[2]
+
+
+# def calc_respm_cod(acc,bpm_idx,hcm_idx,vcm_idx,symmetry=1,printing=False):
+#     """
+#
+#     """
+#
+#     def get_response_matrix(bpms, hcms, vcms):
+#         # M(y,x) --> y : orbit    x: corrector
+#         M, T = _pyaccel.tracking.findm66(acc, 1:length(the_ring)+1);
+#
+#
+#         len_hcms = len(hcms)
+#         len_vcms = len(vcms)
+#         len_bpms = len(bpms)
+#
+#         mxx = _np.zeros((len_bpms, len_hcms))
+#         myx = _np.zeros((len_bpms, len_hcms))
+#         mxy = _np.zeros((len_bpms, len_vcms))
+#         myy = _np.zeros((len_bpms, len_vcms))
+#
+#         len_hcms = [sum([acc[j].length for j in hcms[i]]) for i in range(len(hcms))]
+#         len_vcms = [sum([acc[j].length for j in vcms[i]]) for i in range(len(vcms))]
+#
+#         D = diag(ones(1,size(M,1)));
+#         for i=1:len_bpms
+#             R_i = T(:,:,bpms(i,end));
+#             DM_i = (D - R_i * M / R_i);
+#             for j=1:len_hcms
+#                 [mxx(i,j), myx(i,j), ~, ~] = get_C(T,DM_i, R_i, bpms(i,:),hcms(j,:),total_hcms(j));
+#
+#             for j=1:len_vcms
+#                 [~, ~, mxy(i,j), myy(i,j)] = get_C(T,DM_i, R_i, bpms(i,:),vcms(j,:),total_vcms(j));
+#
+#
+#         MR = [mxx, mxy; myx, myy];
+#
+#         def get_C(T,DM_i, R_i, i,j, length):
+#             # cxy --> orbit at bpm x due to kick in corrector y
+#
+#             #R_j = T(:,:,j(end));
+#             if (i(end)>j(end))
+#                 R_ij = R_i/T(:,:,j(end)); % R_i/R_j
+#             else
+#                 R_ij = R_i * (T(:,:,end) / T(:,:,j(end)));
+#
+#             #C = R_ij / (diag([1 1 1 1])-M44);
+#             C = DM_i \ R_ij
+#
+#             cxx = -(length/2)*C[0,0]   +   C[0,1]
+#             cyx = -(length/2)*C[2,0]   +   C[2,1]
+#             cxy = -(length/2)*C[0,2]   +   C[0,3]
+#             cyy = -(length/2)*C[2,2]   +   C[2,3]
+#
+#
+#     # making sure they are in order
+#     bpm_idx = sorted(bpm_idx)
+#     hcm_idx = sorted(hcm_idx)
+#     vcm_idx = sorted(vcm_idx)
+#
+#
+#     nr_bpms = len(bpm_idx)
+#     nr_hcms = len(hcm_idx)
+#     nr_vcms = len(vcm_idx)
+#
+#     if printing:
+#         print('bpms:{0:03d}, hcms:{0:03d}, vcms:{0:03d}'.format(
+#                    nr_bpms, nr_hcms, nr_vcms))
+#
+#
+#     Mxx = _np.zeros(nr_bpms, nr_hcms)
+#     Myx = _np.zeros(nr_bpms, nr_hcms)
+#     Mxy = _np.zeros(nr_bpms, nr_vcms)
+#     Myy = _np.zeros(nr_bpms, nr_vcms)
+#
+#     len_bpm = nr_bpms // symmetry
+#     len_hcm = nr_hcms // symmetry
+#     len_vcm = nr_vcms // symmetry
+#     if (nr_bpm % 2) or (nr_hcm % 2) or (len_vcm % 2):
+#         len_bpm = nr_bpm
+#         len_hcm = nr_hcm
+#         len_vcm = nr_vcm
+#         symmetry = 1
+#     else:
+#         hcm_idx = hcm_idx[:len_hcm]
+#         vcm_idx = vcm_idx[:len_vcm]
+#
+#
+#     M = get_response_matrix(the_ring, bpm_idx, hcm_idx, vcm_idx);
+#     M = mat2cell(M, [nr_bpms,nr_bpms],[len_hcm, len_vcm]);
+#     mxx = M{1,1};
+#     mxy = M{1,2};
+#     myx = M{2,1};
+#     myy = M{2,2};
+#
+#     for i=0:(symmetry-1)
+#         indcs = (i*len_hcm+1):((i+1)*len_hcm);
+#         Mxx(:,indcs) = circshift(mxx,len_bpm*i);
+#         Myx(:,indcs) = circshift(myx,len_bpm*i);
+#
+#         indcs = (i*len_vcm+1):((i+1)*len_vcm);
+#         Mxy(:,indcs) = circshift(mxy,len_bpm*i); %the last bpm turns into the first
+#         Myy(:,indcs) = circshift(myy,len_bpm*i);
+#     end
+#
+#     r.respm.mxx = Mxx;
+#     r.respm.mxy = Mxy;
+#     r.respm.myx = Myx;
+#     r.respm.myy = Myy;
+#
+#     [U,S,V] = svd([Mxx Mxy; Myx Myy],'econ');
+#     r.respm.U = U;
+#     r.respm.V = V;
+#     r.respm.S = S;
+#
+#     sv = diag(S);
+#     if print
+#         fprintf('   number of singular values: %03i\n', size(S,1));
+#         fprintf('   singular values: %f,%f,%f ... %f,%f,%f\n', sv(1),sv(2),sv(3),sv(end-2),sv(end-1),sv(end));
+#     end
