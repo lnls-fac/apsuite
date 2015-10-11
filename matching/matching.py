a540a96801049d278ddd2f7aac8974fcb3544f54
fernando <fernandohds564@gmail.com>
2015-10-11 12:56:48 -0300

    Development of structure and documentation of matching module.

--- matching/matching.py
+++ matching/matching.py
@@ -5,9 +5,12 @@ import scipy.optimize as _scyopt
 def matching(acc, objectives=None, constraints=None,variables=None, covariables=None):
     """ Performs the matching of optical functions using least squares.
 
-
     variables : Must be a list of dictionaries with keys:
-        'fam_name': family name of the ring element to be varied;
+        'elements': family name of the ring element to be varied or list of
+            indices of the elements in the lattice.
+        'separated': Optional, Boolean. True if each element is to be treated as
+            independent knobs or False if they should be seen as a family.
+            Default is False.
         'atribute': name of the attribute to be varied.
         'index'   : Optional. In case the attribute is a vector or a matrix,
             this key defines which index of the attribute must be varied. It
@@ -16,6 +19,17 @@ def matching(acc, objectives=None, constraints=None,variables=None, covariables=
         'max' : upper bound for the attribute value. Define None for unbounded (default).
     objectives: List of dictionaries defining the objectives (or penalties) of
         the optimization. Each dictionary must have the keys:
+        'quantities': string or tuple of strings defining which quantities will
+            be used in 'fun'. The full list of possible candidates: 'betax','betay',
+            'alphax','alphay','etax','etay','etaxp','etayp','mux','muy','tunex',
+            'tuney',
+        'where': family name of the elements where to calculate
+        'fun' : function which takes the quantities defined in 'quantities', in that
+            order, and returns a float or numpy_ndarray of the same size as the
+            indices in 'where'
+
+
+        'type': type of the comparison to make
 
 
     """
