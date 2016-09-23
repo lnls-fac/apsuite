922ff12176bdaafbe7d16de06106b89370de5ebe
ximenes <xresende@gmail.com>
2016-09-23 10:11:41 -0300

    Fix file permissions

--- apsuite/__init__.py
+++ apsuite/__init__.py
@@ -2,6 +2,7 @@
 #from . import lattice_errors
 #from . import matching
 from . import trackcpp_utils
+from . import lifetime_measurement
 
 import os as _os
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
