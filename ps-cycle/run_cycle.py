d663ddcc50029f8ab2a68ffba1aa491ab1bbbfb7
ximenes <xresende@gmail.com>
2019-03-07 20:34:51 -0300

    Update ps-cycle

--- ps-cycle/run_cycle.py
+++ ps-cycle/run_cycle.py
@@ -10,44 +10,52 @@ import time
 
 parms = {
     # dt, ampl, nrpts_period, nr_periods, tau_period
-    'BO-Fam:PS-QF': [0.5, 120.0, 50, 4, 1],
-    'BO-Fam:PS-QD': [0.5, 30.0, 50, 4, 1],
-    'BO-Fam:PS-SF': [0.5, 149.0, 50, 4, 1],
-    'BO-Fam:PS-SD': [0.5, 149.0, 50, 4, 1],
-    'BO-Fam:PS-B-2': [0.5, 60.0, 50, 4, 1],
-    'TB-Fam:PS-B': [0.5, 250.0, 50, 4, 1],
-    'TB-01:PS-QD1': [0.5, 10.0, 50, 4, 1],
-    'TB-01:PS-QF1': [0.5, 10.0, 50, 4, 1],
-    'TB-02:PS-QD2A': [0.5, 10.0, 50, 4, 1],
-    'TB-02:PS-QF2A': [0.5, 10.0, 50, 4, 1],
-    'TB-02:PS-QD2B': [0.5, 10.0, 50, 4, 1],
-    'TB-02:PS-QF2B': [0.5, 10.0, 50, 4, 1],
-    'TB-03:PS-QD3': [0.5, 10.0, 50, 4, 1],
-    'TB-03:PS-QF3': [0.5, 10.0, 50, 4, 1],
-    'TB-04:PS-QD4': [0.5, 10.0, 50, 4, 1],
-    'TB-04:PS-QF4': [0.5, 10.0, 50, 4, 1],
-    'TB-01:PS-CH-1': [0.5, 10.0, 50, 4, 1],
-    'TB-01:PS-CV-1': [0.5, 10.0, 50, 4, 1],
-    'TB-01:PS-CH-2': [0.5, 10.0, 50, 4, 1],
-    'TB-01:PS-CV-2': [0.5, 10.0, 50, 4, 1],
-    'TB-02:PS-CH-1': [0.5, 10.0, 50, 4, 1],
-    'TB-02:PS-CV-1': [0.5, 10.0, 50, 4, 1],
-    'TB-02:PS-CH-2': [0.5, 10.0, 50, 4, 1],
-    'TB-02:PS-CV-2': [0.5, 10.0, 50, 4, 1],
-    'TB-04:PS-CH': [0.5, 10.0, 50, 4, 1],
-    'TB-04:PS-CV-1': [0.5, 10.0, 50, 4, 1],
-    'TB-04:PS-CV-2': [0.5, 10.0, 50, 4, 1],
+    'BO-Fam:PS-QF': [0.5, 120.0, 50, 4, 1, False],
+    'BO-Fam:PS-QD': [0.5, 30.0, 50, 4, 1, False],
+    'BO-Fam:PS-SF': [0.5, 149.0, 50, 4, 1, False],
+    'BO-Fam:PS-SD': [0.5, 149.0, 50, 4, 1, False],
+    'BO-Fam:PS-B-2': [0.5, 60.0, 100, 4, 1, False],
+    'BO-02D:PS-QS': [0.5, 10.0, 50, 4, 1, False],
+    'TB-Fam:PS-B': [0.5, 250.0, 50, 4, 1, True],
+    'TB-01:PS-QD1': [0.5, 10.0, 50, 4, 1, False],
+    'TB-01:PS-QF1': [0.5, 10.0, 50, 4, 1, False],
+    'TB-02:PS-QD2A': [0.5, 10.0, 50, 4, 1, False],
+    'TB-02:PS-QF2A': [0.5, 10.0, 50, 4, 1, False],
+    'TB-02:PS-QD2B': [0.5, 10.0, 50, 4, 1, False],
+    'TB-02:PS-QF2B': [0.5, 10.0, 50, 4, 1, False],
+    'TB-03:PS-QD3': [0.5, 10.0, 50, 4, 1, False],
+    'TB-03:PS-QF3': [0.5, 10.0, 50, 4, 1, False],
+    'TB-04:PS-QD4': [0.5, 10.0, 50, 4, 1, False],
+    'TB-04:PS-QF4': [0.5, 10.0, 50, 4, 1, False],
+    'TB-01:PS-CH-1': [0.5, 10.0, 50, 4, 1, False],
+    'TB-01:PS-CV-1': [0.5, 10.0, 50, 4, 1, False],
+    'TB-01:PS-CH-2': [0.5, 10.0, 50, 4, 1, False],
+    'TB-01:PS-CV-2': [0.5, 10.0, 50, 4, 1, False],
+    'TB-02:PS-CH-1': [0.5, 10.0, 50, 4, 1, False],
+    'TB-02:PS-CV-1': [0.5, 10.0, 50, 4, 1, False],
+    'TB-02:PS-CH-2': [0.5, 10.0, 50, 4, 1, False],
+    'TB-02:PS-CV-2': [0.5, 10.0, 50, 4, 1, False],
+    'TB-04:PS-CH': [0.5, 10.0, 50, 4, 1, False],
+    'TB-04:PS-CV-1': [0.5, 10.0, 50, 4, 1, False],
+    'TB-04:PS-CV-2': [0.5, 10.0, 50, 4, 1, False],
 }
 
 
-def gen_waveform(dt, ampl, nrpts_period, nr_periods, tau_period):
+def gen_waveform(dt, ampl, nrpts_period, nr_periods, tau_period, square):
     """."""
     period = dt * nrpts_period
     tau = tau_period*period
     tot_nrpts = nrpts_period * nr_periods
     v = list(range(0, tot_nrpts))
     t = dt * np.array(v)
-    w = ampl * np.sin(2*np.pi*t/period) * np.exp(-t/tau)
+    s = np.sin(2*np.pi*t/period) * np.exp(-t/tau)
+    if square:
+        w = ampl * s**2
+    else:
+        w = ampl * s
+    t = np.append(t, t[-1] + dt)
+    w = np.append(w, 0.0)
+
     return t, w
 
 
