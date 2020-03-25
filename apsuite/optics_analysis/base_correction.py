c7fa829ab670386df6b1313fdde1f2f6c21658ea
murilobalves <alvesb.murilo@gmail.com>
2020-03-25 16:11:40 -0300

    BASE.ENH: Add first version of base correction for optics analysis

---
+++ apsuite/optics_analysis/base_correction.py
@@ -0,0 +1,190 @@
+"""."""
+
+from collections import namedtuple as _namedtuple
+from copy import deepcopy as _dcopy
+import numpy as np
+
+__KNOBTYPES = _namedtuple('KnobTypes', ['Focusing', 'Defocusing'])
+
+
+class KnobTypes(__KNOBTYPES):
+    """."""
+
+    @property
+    def ALL(self):
+        """."""
+        return self.Focusing + self.Defocusing
+
+
+class BaseCorr():
+    """."""
+
+    METHODS = _namedtuple('Methods', ['Additional', 'Proportional'])(0, 1)
+    GROUPING = _namedtuple('Grouping', ['Individual', 'TwoKnobs'])(0, 1)
+    CORR_STATUS = _namedtuple('CorrStatus', ['Fail', 'Sucess'])(0, 1)
+
+    def __init__(self, model, acc, method=None, grouping=None):
+        """."""
+        self.model = model
+        self.acc = acc
+        self._method = BaseCorr.METHODS.Proportional
+        self._grouping = BaseCorr.GROUPING.TwoKnobs
+        self.knobs = None
+        self.fam = None
+        self.method = method
+        self.grouping = grouping
+
+    @property
+    def grouping(self):
+        """."""
+        return self._grouping
+
+    @grouping.setter
+    def grouping(self, value):
+        if value is None:
+            return
+        elif isinstance(value, str):
+            self._grouping = int(value in BaseCorr.GROUPING._fields[1])
+        elif int(value) in BaseCorr.GROUPING:
+            self._grouping = int(value)
+
+    @property
+    def grouping_str(self):
+        """."""
+        return BaseCorr.GROUPING._fields[self._grouping]
+
+    @property
+    def method(self):
+        """."""
+        return self._method
+
+    @method.setter
+    def method(self, value):
+        if value is None:
+            return
+        elif isinstance(value, str):
+            self._method = int(value in BaseCorr.METHODS._fields[1])
+        elif int(value) in BaseCorr.METHODS:
+            self._method = int(value)
+
+    @property
+    def method_str(self):
+        """."""
+        return BaseCorr.METHODS._fields[self._method]
+
+    def define_knobs(self, f_knobs=None, d_knobs=None):
+        """."""
+        self.knobs = KnobTypes(Focusing=f_knobs, Defocusing=d_knobs)
+
+    def get_parameter(self, model=None):
+        """."""
+        raise NotImplementedError
+
+    def calc_jacobian_matrix(self, model=None):
+        """."""
+        raise NotImplementedError
+
+    def get_family_data(self):
+        """."""
+        raise NotImplementedError
+
+    def get_strength(self, model=None, knobs=None, strength_type=None):
+        """."""
+        if model is None:
+            model = self.model
+        if knobs is None:
+            knobs = self.knobs.ALL
+        stren = []
+        for knb in knobs:
+            stren_mag = []
+            for mag in self.fam[knb]['index']:
+                stren_seg = []
+                for seg in mag:
+                    stren_seg.append(getattr(model[seg], strength_type))
+                stren_mag.append(sum(stren_seg))
+            stren.append(np.mean(stren_mag))
+        return np.array(stren)
+
+    def correct_parameters(self,
+                           goal_parameters,
+                           model=None,
+                           param_matrix=None,
+                           tol=1e-6,
+                           nr_max=10,
+                           nsv=None):
+        """."""
+        if model is None:
+            model = self.model
+        if self.method not in BaseCorr.METHODS:
+            raise Exception('Invalid correction method!')
+
+        if param_matrix is None:
+            param_mat = self.calc_jacobian_matrix(model)
+        else:
+            param_mat = _dcopy(param_matrix)
+
+        nominal_stren = self.get_strength(model)
+        if self._method == BaseCorr.METHODS.Proportional:
+            param_mat *= nominal_stren
+        if self._grouping == BaseCorr.GROUPING.TwoKnobs:
+            param_mat = self._group_2knobs_matrix(param_mat)
+
+        U, S, V = np.linalg.svd(param_mat, full_matrices=False)
+        iS = 1/S
+        iS[np.isnan(iS)] = 0
+        iS[np.isinf(iS)] = 0
+        if nsv is not None:
+            iS[nsv:] = 0
+        iS = np.diag(iS)
+        invmat = -1 * np.dot(np.dot(V.T, iS), U.T)
+        param_new = self.get_parameter(model)
+        dparam = param_new - goal_parameters
+        if np.sum(dparam*dparam) < tol:
+            return BaseCorr.CORR_STATUS.Sucess
+
+        for _ in range(nr_max):
+            dstren = np.dot(invmat, dparam)
+            self._add_delta_stren(dstren, model=model)
+            param_new = self.get_parameter(model)
+            dparam = param_new - goal_parameters
+            if np.sum(dparam*dparam) < tol:
+                break
+        else:
+            return BaseCorr.CORR_STATUS.Fail
+        return BaseCorr.CORR_STATUS.Sucess
+
+    def _add_delta_stren(self, delta_stren, model=None, strength_type=None):
+        """."""
+        if model is None:
+            model = self.model
+
+        for idx_knb, knb in enumerate(self.knobs.ALL):
+            if self._grouping == BaseCorr.GROUPING.TwoKnobs:
+                if knb in self.knobs.Focusing:
+                    delta = delta_stren[0]
+                elif knb in self.knobs.Defocusing:
+                    delta = delta_stren[1]
+            else:
+                delta = delta_stren[idx_knb]
+            for mag in self.fam[knb]['index']:
+                for seg in mag:
+                    stren = getattr(model[seg], strength_type)
+                    if self._method == BaseCorr.METHODS.Proportional:
+                        stren *= (1 + delta/len(mag))
+                    else:
+                        stren += delta/len(mag)
+                    setattr(model[seg], strength_type, stren)
+
+    def _group_2knobs_matrix(self, param_matrix=None):
+        """."""
+        if param_matrix is None:
+            param_matrix = self.calc_jacobian_matrix(self.model)
+
+        param_2knobs_matrix = np.zeros((2, 2))
+        nfocus = len(self.knobs.Focusing)
+
+        for nf, _ in enumerate(self.knobs.Focusing):
+            param_2knobs_matrix[:, 0] += param_matrix[:, nf]
+        for ndf, _ in enumerate(self.knobs.Defocusing):
+            param_2knobs_matrix[:, 1] += param_matrix[:, ndf+nfocus]
+        return param_2knobs_matrix
