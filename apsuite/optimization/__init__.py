1b498b84ba0cc6a134e726b9932c1c1abb1f5e5e
Fernando <fernandohds564@gmail.com>
2020-04-15 18:49:53 -0300

    BUG: Fix __init__ files.

--- apsuite/optimization/__init__.py
+++ apsuite/optimization/__init__.py
@@ -2,3 +2,7 @@ from .genetic_algorithm import GA
 from .pso import PSO
 from .scanning import SimpleScan
 from .simulated_annealing import SimulAnneal
+
+del genetic_algorithm, pso, scanning, simulated_annealing
+
+__all__ = ('genetic_algorithm', 'pso', 'scanning', 'simulated_annealing')
