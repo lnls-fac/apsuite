4e5644440ef4272bc942efcc4799756242362319
murilobalves <alvesb.murilo@gmail.com>
2022-03-10 14:17:56 -0300

    COMMISS.ENH: Bugs fixes and code organization in OrbitStability

--- apsuite/commisslib/orbit_stability_analysis.py
+++ apsuite/commisslib/orbit_stability_analysis.py
@@ -1,5 +1,4 @@
 """."""
-from ast import Or
 import numpy as _np
 import matplotlib.pyplot as _plt
 import datetime as _datetime
@@ -14,6 +13,7 @@ class OrbitAnalysis:
     MOM_COMPACT = 1.636e-04
     NR_BPMS_SI = 160
     HARM_NR = 864
+    ENERGY_SPREAD = 0.085  # [%]
     BPM_SWITCHING_FREQ = 12.5e3  # Hz
     FOFB_DOWNSAMPLING = 23
     MONIT1_DOWNSAMPLING = 25
@@ -28,6 +28,7 @@ class OrbitAnalysis:
         self._orm_meas = None
         self._sampling_freq = None
         self.load_orb()
+        self.get_closest_orm()
         self.sampling_freq = self.get_sampling_freq(self.data)
 
     @property
@@ -119,6 +120,7 @@ class OrbitAnalysis:
         stg += gtmp('bpms_switching_mode', data['bpms_switching_mode'], '')
         stg += gtmp('bpms_nrsamples_pre', data['bpms_nrsamples_pre'], '')
         stg += gtmp('bpms_nrsamples_post', data['bpms_nrsamples_post'], '')
+        # print(stg)
         orbx, orby = data['orbx'], data['orby']
         # zero mean in samples dimension
         orbx -= orbx.mean(axis=0)[None, :]
@@ -127,7 +129,7 @@ class OrbitAnalysis:
         self.orbx, self.orby = orbx, orby
 
     def get_closest_orm(self):
-        """."""
+        """Find Orbit Response Matrix measured close to data acquisition."""
         configs = self.orm_client.find_configs()
         delays = []
         for cfg in configs:
@@ -142,6 +144,143 @@ class OrbitAnalysis:
         self.etax, self.etay = etaxy[:self.NR_BPMS_SI], etaxy[self.NR_BPMS_SI:]
         self.orm = orm_meas
 
+    def calc_integrated_spectrum(self, data, inverse=False):
+        """."""
+        spec, freq = self.calc_spectrum(data, fs=self.sampling_freq)
+        spec *= spec
+        if inverse:
+            intpsd = _np.sqrt(2*_np.cumsum(spec[::-1], axis=0))[::-1]
+        else:
+            intpsd = _np.sqrt(2*_np.cumsum(spec, axis=0))
+        return intpsd, freq
+
+    def remove_switching_freq(self):
+        """."""
+        fs = self.sampling_freq
+        fil_orbx, freq = self.filter_matrix(
+            self.orbx, fmin=0, fmax=self.BPM_SWITCHING_FREQ*0.9, fs=fs)
+        fil_orby, _ = self.filter_matrix(
+            self.orby, fmin=0, fmax=self.BPM_SWITCHING_FREQ*0.9, fs=fs)
+        return fil_orbx, fil_orby, freq
+
+    def filter_around_sync_freq(self, central_freq=24*64, window=5):
+        """."""
+        fil_orbx, fil_orby, _ = self.remove_switching_freq()
+        fmin = central_freq - window/2
+        fmax = central_freq + window/2
+        fs = self.sampling_freq
+        fil_orbx, _ = self.filter_matrix(
+            self.orbx, fmin=fmin, fmax=fmax, fs=fs)
+        fil_orby, _ = self.filter_matrix(
+            self.orby, fmin=fmin, fmax=fmax, fs=fs)
+        return fil_orbx, fil_orby
+
+    def energy_stability_analysis(self, central_freq=24*64, window=5):
+        """."""
+        orbx, orby = self.filter_around_sync_freq(
+            central_freq=central_freq, window=window)
+        orbxy_fil = _np.hstack((orbx, orby))
+        etaxy = _np.hstack((self.etax, self.etay))
+        _, _, vhmat = self.calc_pca(orbxy_fil)
+        corrs = []
+        etaxy -= _np.mean(etaxy)
+        for mode in range(vhmat.shape[0]):
+            vech_nomean = vhmat[mode] - _np.mean(vhmat[mode])
+            corrs.append(abs(self.calc_correlation(vech_nomean, etaxy)))
+        maxcorr_idx = _np.argmax(corrs)
+        vheta = vhmat[maxcorr_idx]
+        vheta -= _np.mean(vheta)
+        gamma = _np.dot(etaxy, vheta)/_np.dot(etaxy, etaxy)
+        eta_meas = vheta/gamma
+        orbx_ns, orby_ns, _ = self.remove_switching_freq()
+        orbxy = _np.hstack((orbx_ns, orby_ns))
+        orbxy -= _np.mean(orbxy, axis=1)[:, None]
+        denergy = _np.dot(orbxy, vheta) * gamma
+        return eta_meas, denergy
+
+    def plot_energy_spectrum(self, denergy, label='', figname=''):
+        """."""
+        energy_spec, freq = self.calc_spectrum(denergy, fs=self.sampling_freq)
+        fig = _plt.figure(figsize=(18, 6))
+        _plt.plot(freq, energy_spec, label=label)
+
+        rfreq = round(_np.max(freq)/60)
+        ripple = _np.arange(0, rfreq) * 60
+        jfreq = round(_np.max(freq)/64)
+        rfjitt = _np.arange(0, jfreq) * 64
+
+        for idx, rip in enumerate(ripple):
+            lab = ''
+            if not idx:
+                lab = r'n $\times$ 60Hz'
+            _plt.axvline(
+                x=rip, ls='--', lw=1, label=lab, color='k')
+        for idx, jit in enumerate(rfjitt):
+            lab = ''
+            if not idx:
+                lab = r'n $\times$ 64Hz'
+            _plt.axvline(
+                x=jit, ls='--', lw=2, label=lab, color='tab:red')
+
+        _plt.xlabel('Frequency [Hz]')
+        _plt.ylabel(r'Amplitude for DFT of $\delta(t)$')
+        _plt.legend(
+            loc='upper right', bbox_to_anchor=(1.12, 1.02), prop={'size': 14})
+        _plt.xlim([0, 5000])
+        _plt.ylim([1e-10, 1e-2])
+        _plt.yscale('log')
+        _plt.grid(False)
+        _plt.tight_layout()
+        if figname:
+            _plt.savefig(figname, dpi=300, format='pdf')
+        _plt.show()
+        return fig
+
+    def plot_energy_integrated_psd(
+            self, denergy, inverse=True, title='', label='', figname=''):
+        """."""
+        fig = _plt.figure(figsize=(12, 6))
+        intpsd, freq = self.calc_integrated_spectrum(
+            denergy, inverse=inverse)
+        freq = freq/1e3
+        _plt.plot(freq, intpsd, label=label)
+
+        rfreq = round(_np.max(freq)/60)
+        ripple = _np.arange(0, rfreq) * 60
+        jfreq = round(_np.max(freq)/64)
+        rfjitt = _np.arange(0, jfreq) * 64
+
+        for idx, rip in enumerate(ripple):
+            lab = ''
+            if not idx:
+                lab = r'n $\times$ 60Hz'
+            _plt.axvline(
+                x=rip, ls='--', lw=1, label=lab, color='k')
+        for idx, jit in enumerate(rfjitt):
+            lab = ''
+            if not idx:
+                lab = r'n $\times$ 64Hz'
+            _plt.axvline(
+                x=jit, ls='--', lw=2, label=lab, color='tab:red')
+        # _plt.xscale('log')
+        _plt.grid(False)
+        _plt.axhline(
+            OrbitAnalysis.ENERGY_SPREAD*0.1,
+            ls='--', label=r'10$\%$ of $\sigma_{\delta}$', color='k')
+        _plt.legend(
+            loc='upper right', bbox_to_anchor=(1.25, 1.02), prop={'size': 14})
+        _plt.title(title)
+        _plt.xlabel('Frequency [kHz]')
+        _plt.ylabel(r'Sqrt of Int. Spec. [$\%$]')
+        _plt.xlim([0.1, 3])
+        _plt.tight_layout()
+        if figname:
+            _plt.savefig(figname, dpi=300, format='pdf')
+        _plt.show()
+        return fig
+
+    # static methods
+    @staticmethod
     def filter_matrix(matrix, fmin=0, fmax=None, fs=1):
         """."""
         if fmax is None:
@@ -151,12 +290,6 @@ class OrbitAnalysis:
         idcs = (freq < fmin) | (freq > fmax)
         dft[idcs] = 0
         return _np.fft.irfft(dft, axis=0), freq
-
-    def calc_dft(vec, fs):
-        """."""
-        dft = _np.fft.rfft(vec)/vec.shape[0]
-        freq = _np.fft.rfftfreq(vec.shape[0], d=1/fs)
-        return dft, freq
 
     @staticmethod
     def get_sampling_freq(data):
@@ -175,16 +308,6 @@ class OrbitAnalysis:
         spec = _np.abs(dft)/data.shape[0]
         return spec, freq
 
-    def calc_integrated_spectrum(self, data, inverse=False):
-        """."""
-        spec, freq = self.calc_spectrum(data, fs=self.sampling_freq)
-        spec *= spec
-        if inverse:
-            intpsd = _np.sqrt(2*_np.cumsum(spec[::-1], axis=0))[::-1]
-        else:
-            intpsd = _np.sqrt(2*_np.cumsum(spec, axis=0))
-        return intpsd, freq
-
     @staticmethod
     def calc_pca(data):
         """."""
@@ -195,106 +318,3 @@ class OrbitAnalysis:
     def calc_correlation(vec1, vec2):
         """."""
         return _np.corrcoef(vec1, vec2)[0, 1]
-
-    def remove_switching_freq(self):
-        """."""
-        fil_orbx, freq = self.filter_data(
-            self.orbx, fmin=0, fmax=self.BPM_SWITCHING_FREQ*0.9)
-        fil_orby, _ = self.filter_data(
-            self.orby, fmin=0, fmax=self.BPM_SWITCHING_FREQ*0.9)
-        return fil_orbx, fil_orby, freq
-
-    def filter_around_sync_freq(self, central_freq=24*64, window=5):
-        """."""
-        fil_orbx, fil_orby, freq = self.remove_switching_freq()
-        fmin = central_freq - window
-        fmax = central_freq + window
-        fil_orbx, freq = self.filter_data(fil_orbx, fmin=fmin, fmax=fmax)
-        fil_orby, _ = self.filter_data(fil_orby, fmin=fmin, fmax=fmax)
-        return fil_orbx, fil_orby, freq
-
-    def energy_stability_analysis(self, central_freq=24*64, window=5):
-        """."""
-        orbx, orby, _ = self.filter_around_sync_freq(
-            central_freq=central_freq, window=window)
-        orbxy = _np.hstack((orbx, orby))
-        etaxy = _np.hstack((self.etax, self.etay))
-        _, _, vhmat = self.calc_pca(orbxy)
-        corrs = []
-        for mode in range(vhmat.shape[0]):
-            corrs.append(self.calc_correlation(vhmat[mode], etaxy))
-        maxcorr_idx = _np.argmax(corrs)
-        vheta = vhmat[maxcorr_idx]
-        gamma = _np.dot(etaxy, vheta)/_np.dot(etaxy, etaxy)
-        eta_meas = vheta/gamma
-        denergy = _np.dot(orbxy, vheta) * gamma
-        return eta_meas, denergy
-
-    def plot_energy_spectrum(self, denergy, label='', figname=''):
-        """."""
-        energy_dft, freq = self.calc_dft(denergy, fs=self.sampling_freq)
-        _plt.figure(figsize=(18, 6))
-        _plt.plot(freq, _np.abs(energy_dft), label=label)
-
-        rfreq = round(_np.max(freq)/60)
-        ripple = _np.arange(0, rfreq) * 60
-        jfreq = round(_np.max(freq)/64)
-        rfjitt = _np.arange(0, jfreq) * 64
-
-        _plt.vlines(
-            x=ripple, ymin=1e-10, ymax=2e-5,
-            ls='--', lw=1.0, label=r'n $\times$ 60Hz')
-        _plt.vlines(
-            x=rfjitt, ymin=1e-10, ymax=2e-5,
-            ls='--', lw=2, label=r'n $\times$ 64Hz', color='tab:red')
-        _plt.xlabel('Frequency [Hz]')
-        _plt.ylabel(r'Amplitude for DFT of $\delta(t)$')
-        _plt.legend(
-            loc='upper right', bbox_to_anchor=(1.12, 1.02), prop={'size': 14})
-        # _plt.xlim([0, 500])
-        # _plt.ylim([1e-10, 2e-5])
-        _plt.yscale('log')
-        _plt.grid(False)
-        _plt.tight_layout()
-        _plt.savefig(figname, dpi=300, format='pdf')
-        _plt.show()
-
-    def plot_integrated_psd(
-            self, denergy, label='', figname='', inverse=False):
-        """."""
-        _plt.figure(figsize=(12, 6))
-        intpsd, freq = self.calc_integrated_spectrum(
-            denergy, inverse=inverse, fs=self.sampling_freq)
-        _plt.plot(freq/1e3, intpsd*100, label=label)
-
-        rfreq = round(_np.max(freq)/60)
-        ripple = _np.arange(0, rfreq) * 60
-        jfreq = round(_np.max(freq)/64)
-        rfjitt = _np.arange(0, jfreq) * 64
-
-        _plt.vlines(
-            x=ripple/1e3,
-            ymin=_np.min(intpsd)*100/0.5, ymax=_np.max(intpsd)*100*1.5,
-            ls='--', lw=1, label=r'n $\times$ 60Hz', color='k')
-        _plt.vlines(
-            x=rfjitt/1e3,
-            ymin=_np.min(intpsd)*100/0.5, ymax=_np.max(intpsd)*100*1.5,
-            ls='--', lw=2, label=r'n $\times$ 64Hz', color='red')
-        _plt.xscale('log')
-        _plt.grid(False)
-        energy_spread = 0.085  # [%]
-        _plt.hlines(
-            energy_spread*0.1, freq.min()/1e3, freq.max()/1e3, ls='--',
-            label=r'10$\%$ of $\sigma_{\delta}$')
-        _plt.legend(
-            loc='upper right', bbox_to_anchor=(1.25, 1.02), prop={'size': 14})
-
-        title = r'Sqrt of Integrated Spectrum for energy deviation $\delta$'
-        title += '\n'
-        title += r'Beam response analyzed around 12 $\times$ 64 Hz = 768Hz'
-        _plt.title(title)
-        _plt.xlabel('Frequency [kHz]')
-        _plt.ylabel(r'Sqrt of Int. Spec. [\%]')
-        _plt.tight_layout()
-        _plt.savefig(figname, dpi=300, format='pdf')
-        _plt.show()
