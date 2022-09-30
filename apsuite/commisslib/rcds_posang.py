7bd8c34714e2499598fb581ebe09e520432c4a6d
murilobalves <alvesb.murilo@gmail.com>
2022-09-30 15:03:04 -0300

    COMMISS.ENH: First version of class to test RCDS optimization of PosAng

--- apsuite/commisslib/rcds_posang.py
+++ apsuite/commisslib/rcds_posang.py
@@ -2,96 +2,88 @@
 # import matplotlib.pyplot as _plt
 import time as _time
 import numpy as _np
-from ..utils import ThreadedMeasBaseClass as _BaseClass, \
-    ParamsBaseClass as _ParamsBaseClass
+from ..utils import ThreadedMeasBaseClass as _BaseClass
 from siriuspy.devices import PosAng, CurrInfoSI, EVG, PowerSupplyPU, \
     InjSysPUModeHandler
 from ..optimization.rcds import RCDS as _RCDS, RCDSParams as _RCDSParams
 
 
-class OptimizePosAngParams(_ParamsBaseClass, _RCDSParams):
-    """."""
-
-    def __init__(self):
-        """."""
-        _ParamsBaseClass().__init__()
-        _RCDSParams().__init__()
-
-    def __str__(self):
-        """."""
-        stg = _RCDSParams().__str__()
-        return stg
-
-
-class OptimizePosAngPosAng(_RCDS, _BaseClass):
+class OptimizePosAng(_RCDS, _BaseClass):
     """."""
 
     def __init__(self, isonline=True, use_thread=True):
         """."""
         _BaseClass.__init__(
-            self, params=OptimizePosAngParams(), isonline=isonline)
+            self, params=_RCDSParams(), isonline=isonline)
         _RCDS.__init__(self, use_thread=use_thread)
-        if isonline:
+        if self.isonline:
             self._create_devices()
 
-    def objective_function(self, pos):
+    def objective_function(self, pos, apply=True):
         """."""
-        # apply variations to machine
-        self.apply_changes(pos)
-        # inject and measure injeff
+        if apply:
+            self.apply_changes(pos)
         injeff = self._inject()
         return -injeff
 
-    def measure_noise_level(self, nr_evals):
+    def measure_objective_function_noise(self, nr_evals, pos=None):
         """."""
-        effs = []
-        print('measuring noise level:')
+        if pos is None:
+            pos = self.params.initial_position
+        obj = []
         for _ in range(nr_evals):
-            effs.append(self._inject())
-        noise_level = _np.std(effs)
+            obj.append(self.objective_function(pos, apply=False))
+        noise_level = _np.std(obj)
         self.params.noise_level = noise_level
-        return noise_level
+        self.data['measured_objfuns_for_noise'] = obj
+        self.data['measured_noise_level'] = noise_level
+        return noise_level, obj
 
     def initialization(self):
         """."""
-        if self.isonline:
-            posang = self.devices['posang']
-            posx0 = posang.delta_posx
-            angx0 = posang.delta_angx
-            posy0 = posang.delta_posy
-            angy0 = posang.delta_angy
-            kick_nlk0 = self.devices['nlk'].strength
-            posang.cmd_update_reference()
+        posang = self.devices['posang']
+        pos0 = self.get_positions_machine()
+        posx0, angx0, posy0, angy0, kick_nlk0 = pos0
+        posang.cmd_update_reference()
 
-            evg = self.devices['evg']
-            # inject on first bucket just once
-            evg.bucket_list = [1]
-            evg.nrpulses = 1
-            evg.cmd_update_events()
-            _time.sleep(1)
+        evg = self.devices['evg']
+        # inject on first bucket just once
+        evg.bucket_list = [1]
+        evg.nrpulses = 1
+        evg.cmd_update_events()
+        _time.sleep(1)
+        self.devices['injsys'].cmd_switch_to_optim()
 
-            self.devices['injsys'].cmd_switch_to_optim()
+        self.data['timestamp'] = _time.time()
+        self.data['posx0'] = posx0
+        self.data['angx0'] = angx0
+        self.data['posy0'] = posy0
+        self.data['angy0'] = angy0
+        self.data['kick_nlk0'] = kick_nlk0
 
-            self.data['timestamp'] = _time.time()
-            self.data['posx0'] = posx0
-            self.data['angx0'] = angx0
-            self.data['posy0'] = posy0
-            self.data['angy0'] = angy0
-            self.data['kick_nlk0'] = kick_nlk0
+    def get_positions_machine(self):
+        """."""
+        posang = self.devices['posang']
+        posx = posang.delta_posx
+        angx = posang.delta_angx
+        posy = posang.delta_posy
+        angy = posang.delta_angy
+        kick_nlk = self.devices['nlk'].strength
+        pos = _np.array([posx, angx, posy, angy, kick_nlk])
+        return pos
 
     def apply_changes(self, pos):
         """."""
         print('applying variations to machine')
-        if self.isonline:
-            posang, nlk = self.devices['posang'], self.devices['nlk']
-            posx, angx, posy, angy, kick_nlk = pos
-            print('     setting new posang')
-            posang.delta_posx = posx
-            posang.delta_angx = angx
-            posang.delta_posy = posy
-            posang.delta_angy = angy
-            print('     setting new nlk strength')
-            nlk.strength = kick_nlk
+        posang, nlk = self.devices['posang'], self.devices['nlk']
+        posx, angx, posy, angy, kick_nlk = pos
+        print('     setting new posang')
+        posang.delta_posx = posx
+        posang.delta_angx = angx
+        posang.delta_posy = posy
+        posang.delta_angy = angy
+        print('     setting new nlk strength')
+        nlk.strength = kick_nlk
 
     def save_optimization_data(self, fname, apply_machine=False):
         """."""
@@ -101,7 +93,6 @@ class OptimizePosAngPosAng(_RCDS, _BaseClass):
 
         if apply_machine:
             self.apply_changes(self.best_positions[-1])
-
         self.save_data(fname)
 
     def _create_devices(self):
