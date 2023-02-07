8a90977298770d4b5b6552305d3f3bbede7c813f
Murilo B. Alves <murilo.alves@lnls.br>
2023-02-07 09:06:41 -0300

    COMMISS.ENH: Improve propagated dispersion measurement to inject

--- apsuite/commisslib/measure_disp_tbbo.py
+++ apsuite/commisslib/measure_disp_tbbo.py
@@ -5,7 +5,7 @@ import numpy as np
 import numpy.polynomial.polynomial as np_poly
 
 import pyaccel as pa
-from siriuspy.devices import SOFB, DevLILLRF
+from siriuspy.devices import SOFB, DevLILLRF, EVG
 
 from ..utils import MeasBaseClass as _BaseClass, \
     ParamsBaseClass as _ParamsBaseClass
@@ -23,9 +23,10 @@ class ParamsDisp(_ParamsBaseClass):
         """."""
         super().__init__()
         self.delta_kly2_amp = -2
-        self.wait_kly2 = 40
+        self.wait_kly2 = 10
         self.timeout_orb = 10
         self.nr_points = 10
+        self.injection_interval = 30
         # self.kly2_excit_coefs = [66.669, 1.098]  # old
         # self.kly2_excit_coefs = [71.90322743, 1.01026423]  # > 2.5nC
         self.kly2_excit_coeffs = [87.56545895, 0.80518365]  # < 2.5nC
@@ -38,6 +39,7 @@ class ParamsDisp(_ParamsBaseClass):
         stg = ftmp('delta_kly2_amp', self.delta_kly2_amp, '')
         stg += ftmp('wait_kly2', self.wait_kly2, '[s]')
         stg += ftmp('timeout_orb', self.timeout_orb, '[s]')
+        stg += ftmp('injection_interval', self.injection_interval, '[s]')
         stg += dtmp('nr_points', self.nr_points, '')
         coeffs_stg = ', '.join([f'{c:f}' for c in self.kly2_excit_coeffs])
         stg += stmp(
@@ -56,7 +58,8 @@ class MeasureDispTBBO(_BaseClass):
             self.devices = {
                 'bo_sofb': SOFB(SOFB.DEVICES.BO),
                 'tb_sofb': SOFB(SOFB.DEVICES.TB),
-                'kly2': DevLILLRF(DevLILLRF.DEVICES.LI_KLY2)}
+                'kly2': DevLILLRF(DevLILLRF.DEVICES.LI_KLY2),
+                'evg': EVG()}
 
     @property
     def energy(self):
@@ -77,28 +80,31 @@ class MeasureDispTBBO(_BaseClass):
             [self.devices['tb_sofb'].trajy, self.devices['bo_sofb'].trajy])
 
     @property
-    def nr_points(self):
+    def traj(self):
         """."""
-        return min(
-            self.devices['tb_sofb'].nr_points,
-            self.devices['bo_sofb'].nr_points)
+        return np.r_[self.trajx, self.trajy]
 
-    @nr_points.setter
-    def nr_points(self, value):
-        self.devices['tb_sofb'].nr_points = int(value)
-        self.devices['bo_sofb'].nr_points = int(value)
-
-    def wait(self, timeout=10):
+    def inject_and_get_traj(self):
         """."""
-        self.devices['tb_sofb'].wait_buffer(timeout=timeout)
-        self.devices['bo_sofb'].wait_buffer(timeout=timeout)
-
-    def reset(self, wait=0):
-        """."""
-        _time.sleep(wait)
-        self.devices['tb_sofb'].cmd_reset()
-        self.devices['bo_sofb'].cmd_reset()
-        _time.sleep(1)
+        evg = self.devices['evg']
+        trajs = []
+        for i in range(self.params.nr_points):
+            traj0 = self.traj
+            evg.cmd_turn_on_injection()
+            t0_ = _time.time()
+            print(
+                f'{i:02d}/{self.params.nr_points:02d} -> ' +
+                'reading initial trajectory...')
+            for _ in range(50):
+                if not np.any(np.isclose(traj0, self.traj)):
+                    break
+                _time.sleep(self.params.timeout_orb/50)
+            else:
+                print('    Timed out waiting traj to update.')
+            print('    Getting trajectory.')
+            trajs.append(self.traj)
+            _time.sleep(self.params.injection_interval - (_time.time() - t0_))
+        return np.mean(trajs, axis=0)
 
     def measure_dispersion(self):
         """."""
@@ -106,30 +112,23 @@ class MeasureDispTBBO(_BaseClass):
         delta = self.params.delta_kly2_amp
         kly2_dev = self.devices['kly2']
 
-        print('reading initial trajectory...')
-        self.reset(3)
-        self.wait(self.params.timeout_orb)
-        orb = [-np.hstack([self.trajx, self.trajy]), ]
-        ene0 = self.energy
+        traj0 = self.inject_and_get_traj()
 
         print('setting new Klystron2 amplitude...')
         origamp = kly2_dev.amplitude
         kly2_dev.amplitude = origamp + delta
+        _time.sleep(1)
         print(f"Klystron2 Amp: {kly2_dev.amplitude:.3f}")
-
-        self.reset(self.params.wait_kly2)
+        _time.sleep(self.params.wait_kly2)
         print('reading new trajectory...')
-        self.reset(3)
-        self.wait(self.params.timeout_orb)
-        orb.append(np.hstack([self.trajx, self.trajy]))
-        ene1 = self.energy
+        trajp = self.inject_and_get_traj()
 
         print('restoring Klystron2 amplitude...')
         kly2_dev.amplitude = origamp
+        _time.sleep(1)
         print(f"Klystron2 Amp: {kly2_dev.amplitude:.3f}")
-        d_ene = ene1/ene0 - 1
         print('finished!')
-        return np.array(orb).sum(axis=0) / d_ene
+        return trajp - traj0
 
     @staticmethod
     def calc_model_dispersionTBBO(model, bpms):
