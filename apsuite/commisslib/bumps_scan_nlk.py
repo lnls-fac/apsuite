6993be2939f0ae787532bcec9cf343925da3ab22
Murilo B. Alves <murilo.alves@lnls.br>
2023-08-19 13:00:11 -0300

    BUMPNLK.ENH: Improve BumpNLK class

--- apsuite/commisslib/bumps_scan_nlk.py
+++ apsuite/commisslib/bumps_scan_nlk.py
@@ -22,34 +22,31 @@ class BumpNLKParams(_ParamsBaseClass):
         self.posy_min = 0  # [um]
         self.posy_max = 0  # [um]
         self.nr_stepsy = 1
-        self.nlk_kick = 0  # [urad]
         self.acq_nrsamples_pre = 0
         self.acq_nrsamples_post = 100
         self.buffer_sloworb = 50
         self.buffer_multiturn = 1
         self.wait_meas = 7  # [s]
-        self.filename = ''
-        self.save_data = True
+        self.orbcorr_nr_iters = 5
+        self.orbcorr_residue = 10  # [um]
 
     def __str__(self):
         """."""
         dtmp = '{0:20s} = {1:9d}\n'.format
         ftmp = '{0:20s} = {1:9.4f}  {2:s}\n'.format
-        stmp = '{0:20s} = {1:}\n'.format
         stg = ftmp('posx_min', self.posx_min, '[um]')
         stg += ftmp('posx_max', self.posx_max, '[um]')
         stg += dtmp('nr_stepsx', self.nr_stepsx)
         stg += ftmp('posy_min', self.posy_min, '[um]')
         stg += ftmp('posy_max', self.posy_max, '[um]')
         stg += dtmp('nr_stepsy', self.nr_stepsy)
-        stg += ftmp('nlk_kick', self.nlk_kick, '[urad]')
         stg += dtmp('acq_nrsamples_pre', self.acq_nrsamples_pre)
         stg += dtmp('acq_nrsamples_post', self.acq_nrsamples_post)
         stg += dtmp('buffer_sloworb', self.buffer_sloworb)
         stg += dtmp('buffer_multiturn', self.buffer_multiturn)
         stg += ftmp('wait_meas', self.wait_meas, '[s]')
-        stg += stmp('filename', self.filename)
-        stg += dtmp('save_data', self.save_data)
+        stg += dtmp('orbcorr_nr_iters', self.orbcorr_nr_iters)
+        stg += ftmp('orbcorr_residue', self.orbcorr_residue, '[um]')
         return stg
 
 
@@ -64,14 +61,14 @@ class BumpNLK(_BaseClass):
             self, params=BumpNLKParams(),
             target=self._do_measure, isonline=isonline)
         self.data = dict()
+        self.reforbx = None
+        self.reforby = None
         if self.isonline:
             self.devices['sofb'] = SOFB(SOFB.DEVICES.SI)
             self.devices['bbbh'] = BunchbyBunch(BunchbyBunch.DEVICES.H)
             self.devices['bbbv'] = BunchbyBunch(BunchbyBunch.DEVICES.V)
             self._configdb = _ConfigDBClient(config_type='si_orbit')
-            self.reforb = self._configdb.get_config_value('ref_orb')
-            self.reforbx = _np.array(self.reforb['x'])
-            self.reforby = _np.array(self.reforb['y'])
+            self.update_reforb()
 
     def get_trajectory_sofb(self):
         """."""
@@ -107,12 +104,14 @@ class BumpNLK(_BaseClass):
             'stored_current': bbbh.dcct.current,
             'spech_mag': bbbh.sram.spec_mag,
             'spech_freq': bbbh.sram.spec_freq,
+            'bbbh_data_mean': bbbh.sram.data_mean,
             'spech_mk1_mag': bbbh.sram.spec_marker1_mag,
             'spech_mk1_tune': bbbh.sram.spec_marker1_tune,
             'trajx': trajx,
 
             'specv_magv': bbbv.sram.spec_mag,
             'specv_freqv': bbbv.sram.spec_freq,
+            'bbbv_data_mean': bbbv.sram.data_mean,
             'specv_mk1_mag': bbbv.sram.spec_marker1_mag,
             'specv_mk1_tune': bbbv.sram.spec_marker1_tune,
             'trajy': trajy,
@@ -120,33 +119,40 @@ class BumpNLK(_BaseClass):
         return data
 
     def implement_bump(
-            self, refx0=None, refy0=None,
-            agx=0, agy=0, psx=0, psy=0, nr_iters=5, residue=1):
+            self, refx0=None, refy0=None, agx=0, agy=0, psx=0, psy=0):
         """."""
         sofb = self.devices['sofb']
         refx0 = refx0 or self.reforbx
         refy0 = refy0 or self.reforby
+        nr_iters = self.params.orbcorr_nr_iter
+        residue = self.params.orbcorr_residue
         nrefx, nrefy = sofb.si_calculate_bumps(
                 refx0, refy0, BumpNLK.DEFAULT_SS,
                 agx=agx, agy=agy, psx=psx, psy=psy)
         sofb.refx, sofb.refy = nrefx, nrefy
         sofb.correct_orbit_manually(nr_iters=nr_iters, residue=residue)
 
-    def _do_measure(self):
+    def update_reforb(self):
+        """."""
+        self.reforb = self._configdb.get_config_value('ref_orb')
+        self.reforbx = _np.array(self.reforb['x'])
+        self.reforby = _np.array(self.reforb['y'])
+
+    def calc_bump_span(self):
+        """."""
         prms = self.params
-        if prms.save_data:
-            self.save_data(fname=prms.filename, overwrite=False)
         posx_span = _np.linspace(prms.posx_min, prms.posx_max, prms.nr_stepsx)
         posy_span = _np.linspace(prms.posy_min, prms.posy_max, prms.nr_stepsy)
+        return posx_span, posy_span
+
+    def _do_measure(self):
+        prms = self.params
+        posx_span, posy_span = self.calc_bump_span()
 
         # zig-zag type of scan in the y plane
         idy, idx = _np.meshgrid(range(prms.nr_stepsy), range(prms.nr_stepsx))
         idy[1::2] = _np.flip(idy[1::2])
         idx, idy = idx.ravel(), idy.ravel()
-
-        self.config_sofb_sloworb()
-        print('Setting initial bump...')
-        self.implement_bump(psx=posx_span[idx[0]], psy=posy_span[idy[0]])
 
         data = list()
         for iter in range(idx.size):
@@ -154,7 +160,7 @@ class BumpNLK(_BaseClass):
             posy = posy_span[idy[iter]]
 
             self.config_sofb_sloworb()
-            self.implement_bump(psx=posx, psy=posy, nr_iters=3)
+            self.implement_bump(psx=posx, psy=posy)
             self.config_sofb_multiturn()
             _time.sleep(prms.wait_meas)
             data.append(self.get_measurement_data())
@@ -165,8 +171,6 @@ class BumpNLK(_BaseClass):
             print(fstr)
             data[-1]['bump'] = (posx, posy)
             self.data = data
-            if prms.save_data:
-                self.save_data(fname=prms.filename, overwrite=True)
             if self._stopevt.is_set():
                 print('Stopping...')
                 break
@@ -174,14 +178,15 @@ class BumpNLK(_BaseClass):
         # return to initial reference orbit
         self.config_sofb_sloworb()
         print('Returning to ref_orb...')
-        self.implement_bump(psx=0, psy=0, nr_iters=10)
+        self.implement_bump(psx=0, psy=0)
         print('Finished!')
 
     def process_data(self):
         """."""
         posx, posy, magx, magy = [], [], [], []
         trajx, trajy = [], []
-        for datai in self.data:
+        data_index = []
+        for idx, datai in enumerate(self.data):
             trjx = datai['trajx'].copy()
             trjx -= trjx.mean(axis=0)[None, :]
             trjy = datai['trajy'].copy()
@@ -192,15 +197,17 @@ class BumpNLK(_BaseClass):
             posy.append(datai['bump'][1])
             magx.append(datai['spech_mk1_mag'])
             magy.append(datai['specv_mk1_mag'])
+            data_index.append(idx)
 
         nsteps = self.params.nr_stepsx
         anly = dict()
-        anly['posx_bump'] = self._reshape_vec(posx, dim=nsteps)
-        anly['posy_bump'] = self._reshape_vec(posy, dim=nsteps)
-        anly['bbbh_mag'] = self._reshape_vec(magx, dim=nsteps)
-        anly['bbbv_mag'] = self._reshape_vec(magy, dim=nsteps)
-        anly['tbt_trajx_std'] = self._reshape_vec(trajx, dim=nsteps)
-        anly['tbt_trajy_std'] = self._reshape_vec(trajy, dim=nsteps)
+        anly['posx_bump'] = self._reshape_vec(posx, axis0_size=nsteps)
+        anly['posy_bump'] = self._reshape_vec(posy, axis0_size=nsteps)
+        anly['bbbh_mag'] = self._reshape_vec(magx, axis0_size=nsteps)
+        anly['bbbv_mag'] = self._reshape_vec(magy, axis0_size=nsteps)
+        anly['tbt_trajx_std'] = self._reshape_vec(trajx, axis0_size=nsteps)
+        anly['tbt_trajy_std'] = self._reshape_vec(trajy, axis0_size=nsteps)
+        anly['data_index'] = self._reshape_vec(data_index, axis0_size=nsteps)
         self.analysis = anly
 
     def plot_bbb_mag(self, plane='HV', shading='auto', cmap='jet'):
@@ -259,7 +266,7 @@ class BumpNLK(_BaseClass):
         return fig, ax1
 
     @staticmethod
-    def _reshape_vec(vec, dim):
-        new_vec = _np.array(vec).reshape((dim, -1))
+    def _reshape_vec(vec, axis0_size):
+        new_vec = _np.array(vec).reshape((axis0_size, -1))
         new_vec[1::2, :] = new_vec[1::2, ::-1]
         return new_vec
