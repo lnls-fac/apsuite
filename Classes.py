dafcf116173e036f01d4bc03f9ce283cc8c86f1b
Thales_note <thalesquadros@gmail.com>
2023-08-28 09:40:07 -0300

    mudança

--- Classes.py
+++ Classes.py
@@ -1,13 +1,13 @@
 from pyaccel.lifetime import Lifetime
 from pyaccel.lattice import get_attribute
-import touschek_pack.functions as tousfunc
+import functions as tousfunc
 import pymodels
 import pyaccel.optics as py_op
 import numpy as _np
 import matplotlib.pyplot as _plt
 
 
-class tous_analysis():
+class Tous_analysis():
 
     # Defining this class I can call any accelerator I want to realize touschek scattering analysis
     # Keep in mind another terms that could be add in the init of the class
@@ -64,74 +64,127 @@ class tous_analysis():
     # essa lista pode ser de elementos específicos em que se deseje estudar a taxa de espalhamento touschek
 
     def __init__(self,accelerator):
-        self.acc = accelerator
-        self.h_pos = get_attribute(self.acc, 'hmax', indices='closed') # getting the vchamber's height
-        self.h_neg = get_attribute(self.acc, 'hmin', indices='closed')
-        self.ltime = Lifetime(self.acc)
-        fam = pymodels.si.get_family_data(self.acc) # Geting the name of the element we desire to study
+        self._acc = accelerator
+        self.h_pos = get_attribute(self._acc, 'hmax', indices='closed') # getting the vchamber's height
+        self.h_neg = get_attribute(self._acc, 'hmin', indices='closed')
+        self.ltime = Lifetime(self._acc)
+        fam = pymodels.si.get_family_data(self._acc) # Geting the name of the element we desire to study
         self.lname = list(fam.keys()) # geting  
-        self.index = tousfunc.el_idx_collector() # this calculation is relativily fast to execute, 
+        self.index = tousfunc.el_idx_collector(self._acc, self.lname) # this calculation is relativily fast to execute, 
+        
+        self._sc_accps = None # check
+        self._accep = None # check        
         off_array = _np.linspace(0,0.46, 460) 
-        self.ener_off = off_array # interval of energy deviation for calculating the amplitudes and idx_limitants from linear model
-        
-        self.sc_accps = None # check
-        self._accep = None # check
-        self.nturns = None # check
-        self.deltas = None  # check 
+        self._ener_off = off_array # interval of energy deviation for calculating the amplitudes and idx_limitants from linear model
+        self._nturns = None # check
+        self._deltas = None  # check 
 
-        self.linear_model_ampp = None
-        self.idx_lim_pos = None
-        self.linear_model_ampn = None
-        self.idx_lim_neg = None
-        
+        self._lamppn_idx = None # this parameter defines 4 analysis' elements calculated by the linear model 
 
 
     # Defining the energy acceptance. This method also needs a setter to change the value of the acceptance by a different inserted model
     @property
     def accep(self):
         if self._accep is None:
-            self._accep = py_op.calc_touschek_energy_acceptance(self.acc)
+            self._accep = py_op.calc_touschek_energy_acceptance(self._acc)
         return self._accep
 
     # Defining the s position, positive and negative accptances along the ring at each 10 meters.
     # the description above is actually the returns of the function
     @property
-    def convert_scalc(self):
-        if self.sc_accps is None:
-            self.sc_accps = tousfunc.get_scaccep(self.acc, self.accep)
-        return self.sc_accps
+    def scalc(self):
+        if self._sc_accps is None:
+            self._sc_accps = tousfunc.get_scaccep(self._acc, self._accep)
+        return self._sc_accps
 
     # Defining the energy array to get the maximum amplitudes and the limitant indices by the linear model
     @property
-    def lin_offs_set(self):
-        return self.ener_off
+    def ener_off(self):
+        return self._ener_off
     
-    @lin_offs_set.setter
-    def lin_offs_set(self, new):
-        self.ener_off = new
-        return self.ener_off
+    @ener_off.setter
+    def ener_off(self, new):
+        self._ener_off = new
+        return self._ener_off
     
-    # Defining the electron number of turns to  
+    # Defining the number of turns to realize the tracking simulation
     @property
-    def nturns_set(self):
-        return self.nturns
+    def nturns(self):
+        return self._nturns
     
-    @nturns_set.setter
-    def nturns_set(self, new_turns):
-        self.nturns = new_turns
-        return self.nturns
+    @nturns.setter
+    def nturns(self, new_turns):
+        self._nturns = new_turns
+        return self._nturns
 
     @property
-    def deltas_set(self):
-        return self.nturns
+    def deltas(self):
+        return self._deltas
     
-    @deltas_set.setter
-    def deltas_set(self, new_deltas):
-        self.nturns = new_deltas
-        return self.nturns
+    @deltas.setter
+    def deltas(self, new_deltas):
+        self._deltas = new_deltas
+        return self._deltas
     
+    # Function below calculates the linear model amplitudes for graphic analysis
+
+    # this function defines too the physical limitants calculate by our linear model
+    # I have to verify if this function is a good programing practice, I dont know if this is the best aproach
+
+
+    # This property is defined in this way to calculate the linear model maximum amplitudes
+    # and to obtain the indices of the physical limitants
+
+    @property
+    def lamppn_idx(self):
+        if self._lamppn_idx is None:
+            model = pymodels.si.create_accelerator()
+            model.cavity_on = False
+            model.radiation_on = False
+            self.lmd_amp_pos, self.idx_lim_pos = Tous_analysis.calc_amp(model, self.ener_off, self.h_pos, self.h_neg)
+            self.lmd_amp_neg, self.idx_lim_neg = Tous_analysis.calc_amp(model, -self.ener_off, self.h_pos, self.h_neg)
+        
+        return self.lmd_amp_pos, self.idx_lim_pos, self.lmd_amp_neg, self.idx_lim_neg
+
+
+
+    # agora é hora de definir outras propriedades que serão utilizadas nesta classe para realizar a contabilização 
+    # dos devios de energia mais relevantes para o projeto
 
     
+    # this function will calculate the weight factor to get the most important
+    # energy deviation in a touschek scattering process
+
+    # this function must take two functions of my previous code
+    # all I need to do is take f_arg_mod and track_electrons 
+    # Is important to note that track_electrons is based on the nominal model
+    # in this way I must convert the index of the elements from spos calculated by pyaccel
+    # and get the index that aproximates scalc to the spos positions
+
+    #o que eu vou precisar para fazer esta função
+    #com certeza eu vou precisar tomar a aceitancia da maquina para o modelo passado no 
+    #inicio da classe e então selecionar o valor para realizar o corte de desvio de energia 
+    # 
+    
+    def return_tracked(self,s_position, par):
+        if 'pos' in par:
+            res, ind = tousfunc.trackm_elec(self._acc,self._deltas,self._nturns,s_position)
+        elif 'neg' in par:
+            res, ind = tousfunc.trackm_elec(self._acc,-self._deltas,self._nturns,s_position)
+        
+        return res, ind
+    
+    #this function must pass the s_position for the calculation and the weighting the energy deviations
+    # and this function will use the functions that I'm defining in this class to get 
+    # the tracked energy deviations already weighted by the touschek scattering piwinski
+    # distribution
+
+    #for example:
+    #    fast_aquisition(s_position, par) will use return_tracked
+
+    def fast_aquisition(self):
+        pass
+
 
     
     # tudo bem que desenvolver um código que obtenha diversos trackings para todos os pontos do anel é de fato uma coisa legal
@@ -140,15 +193,6 @@ class tous_analysis():
     # e é possível definir funções que vão realizar simulações mais demoradas (provavelmente neste caso será realizada uma sondagem ao longo
     # de todo anel de armazenamento)
 
-    # tenho de pensar em como realizar essas simulações rápidas, uma vez que eu já possuo 
-    
-
-
-    # @property
-    # def define_nturns(self):
-    #     if 
-
-        
-
-
+    # tenho de pensar em como realizar essas simulações rápidas, uma vez que eu já possuo as funções e como 
+    # e como podemos saber em quais pontos específicos é bom realizar esta análise
 
