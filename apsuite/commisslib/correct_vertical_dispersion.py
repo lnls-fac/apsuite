cd52a9cdeeab4b83db1b1f050f6976647ba58065
Murilo B. Alves <murilo.alves@lnls.br>
2023-10-17 10:40:48 -0300

    COMMISSLIB.DISPY.ENH: Improve vertical dispersion correction using CV kicks

--- apsuite/commisslib/correct_vertical_dispersion.py
+++ apsuite/commisslib/correct_vertical_dispersion.py
@@ -13,7 +13,7 @@ from mathphys.functions import get_namedtuple as _get_namedtuple
 class CorrectVerticalDispersionParams(_ParamsBaseClass):
     """."""
 
-    CORR_METHOD = _get_namedtuple('CorrMethod', ['QS', 'CV'])
+    CORR_METHOD = _get_namedtuple('METHODS', ['QS', 'CV'])
 
     def __init__(self):
         """."""
@@ -22,107 +22,110 @@ class CorrectVerticalDispersionParams(_ParamsBaseClass):
         self.nr_points_sofb = 20  # [Hz]
         self.nr_iters_corr = 1
         self.nr_svals = 100  # all qs
-        self.ks_amp_factor2apply = 1
+        self.amp_factor2apply = 1
         self.qs2use_idx = _np.arange(0, 100)
         self.cv2use_idx = _np.arange(0, 160)
-        self._corr_method = self.CORR_METHOD.CV
+        self._corr_method = CorrectVerticalDispersionParams.CORR_METHOD.CV
 
     def __str__(self):
         """."""
         dtmp = '{0:25s} = {1:9d}\n'.format
         ftmp = '{0:25s} = {1:9.2f}  {2:s}\n'.format
-        # stmp = '{0:25s} = {1:9s}  {2:s}\n'.format
         stg = ftmp('delta_rf_freq', self.delta_rf_freq, '[Hz]')
         stg += dtmp('nr_points_sofb', self.nr_points_sofb)
         stg += dtmp('nr_iters_corr', self.nr_iters_corr)
         stg += dtmp('nr_svals', self.nr_svals)
+        stg += ftmp('amp_factor2apply', self.amp_factor2apply, '')
         if self.corr_method == self.CORR_METHOD.QS:
-            stg += ftmp('ks_amp_factor2apply', self.ks_amp_factor2apply, '')
             stg += '{0:25s} = ['.format('qs2use_idx')
             stg += ','.join(f'{idx:3d}' for idx in self.qs2use_idx) + ']'
         else:
-            pass # mod print string
+            pass
         return stg
+
 
     @property
     def corr_method(self):
         """."""
+        """."""
         return self._corr_method
 
     @corr_method.setter
-    def corr_method(self, method_str):
-        if method_str == 'QS':
-            self.params.corr_method =\
-                  CorrectVerticalDispersionParams.CORR_METHOD.QS
-        elif method_str == 'CV':
-            self.params.corr_method =\
-                  CorrectVerticalDispersionParams.CORR_METHOD.CV
-        else:
-            raise ValueError('The method should be "QS" or "CV"')
+    def corr_method(self, value):
+        if value is None:
+            return
+        _prms_mthd = CorrectVerticalDispersionParams.CORR_METHOD
+        if isinstance(value, str):
+            self._corr_method = int(value in _prms_mthd._fields[1])
+        elif int(value) in _prms_mthd:
+            self._corr_method = int(value)
 
 
 class CorrectVerticalDispersion(_ThreadedMeasBaseClass):
     """."""
 
-    def __init__(self, isonline=True):
+    def __init__(self, params, isonline=True):
         """."""
-        super().__init__(target=self._meas_func, isonline=isonline)
+        super().__init__(
+            target=self._meas_func, isonline=isonline, params=params)
         self.model = si.create_accelerator()
         self.fam_data = si.get_family_data(self.model)
         self.model_alpha = pyaccel.optics.get_mcf(self.model)
-        self.model_ks_dispmat = None
-        self.meas_ks_dispmat = None
-        self.model_vk_dispmat = None # already calculated: "dispy_DRM.txt" (160 cv X 160 bpm)
+        self.dispmat = None
         self.orm = OrbRespmat(model=self.model, acc='SI', dim='6d')
-        self.qs_names = None # to avoid errors
+        self.qs_names = None  # to avoid errors
         if self.isonline:
             self.devices['rfgen'] = RFGen(
                 props2init=['GeneralFreq-SP', 'GeneralFreq-RB'])
             self.devices['sofb'] = SOFB(SOFB.DEVICES.SI)
             self.devices['tune'] = Tune(Tune.DEVICES.SI)
-            self.create_skews() # check the corr_method first (inside func)
+            if self.params.corr_method == \
+                    CorrectVerticalDispersionParams.CORR_METHOD.QS:
+                self._create_skews()
 
-    def create_skews(self):
+    def _create_skews(self):
         """."""
-        if self.params.corr_method != \
-                CorrectVerticalDispersionParams.CORR_METHOD.QS:
-            return
         qs_names = _np.array(self.fam_data['QS']['devnames'], dtype=object)
         self.qs_names = qs_names
         for name in self.qs_names:
             self.devices[name] = PowerSupply(name)
 
-    def get_vertical_kicks(self):
-        """Return vertical kicks of sofb-CV."""
-        sofb = self.devices['sofb']
-        return _np.array(sofb.kickcv).ravel()
-
-    def get_skew_strengths(self):
+    def get_strengths(self):
         """."""
-        if self.params.corr_method != \
-                CorrectVerticalDispersionParams.CORR_METHOD.QS:
-            return None
-        names = self.qs_names[self.params.qs2use_idx]
-        stren = [self.devices[name].strength for name in names]
-        return _np.array(stren)
+        _corr_mthd = CorrectVerticalDispersionParams.CORR_METHOD
+        _prms = self.params
+        if _prms.corr_method == _corr_mthd.QS:
+            names = self.qs_names[self.params.qs2use_idx]
+            stren = [self.devices[name].strength for name in names]
+            return _np.array(stren)
+        elif _prms.corr_method == _corr_mthd.CV:
+            return self.devices['sofb'].kickcv
+        return None
 
-    def apply_skew_strengths(self, strengths, factor=1):
+    def apply_strengths(self, strengths, factor=1):
         """."""
-        if self.params.corr_method != \
-                CorrectVerticalDispersionParams.CORR_METHOD.QS:
-            return
-        names = self.qs_names[self.params.qs2use_idx]
-        for idx, name in enumerate(names):
-            self.devices[name].strength = factor * strengths[idx]
+        if self.params.corr_method == \
+                CorrectVerticalDispersionParams.CORR_METHOD.CV:
+            sofb = self.devices['sofb']
+            kick0 = sofb.kickcv
+            sofb.deltakickcv = factor*(strengths - kick0)
+            sofb.cmd_applycorr_cv()
+        else:
+            names = self.qs_names[self.params.qs2use_idx]
+            for idx, name in enumerate(names):
+                self.devices[name].strength = factor * strengths[idx]
 
-    def apply_skew_delta_strengths(self, delta_strengths, factor=1):
+    def apply_delta_strengths(self, delta_strengths, factor=1):
         """."""
-        if self.params.corr_method != \
-                CorrectVerticalDispersionParams.CORR_METHOD.QS:
-            return
-        names = self.qs_names[self.params.qs2use_idx]
-        for idx, name in enumerate(names):
-            self.devices[name].strength += factor * delta_strengths[idx]
+        if self.params.corr_method == \
+                CorrectVerticalDispersionParams.CORR_METHOD.CV:
+            sofb = self.devices['sofb']
+            sofb.deltakickcv = factor*delta_strengths
+            sofb.cmd_applycorr_cv()
+        else:
+            names = self.qs_names[self.params.qs2use_idx]
+            for idx, name in enumerate(names):
+                self.devices[name].strength += factor * delta_strengths[idx]
 
     def get_orb(self):
         """."""
@@ -147,123 +150,109 @@ class CorrectVerticalDispersion(_ThreadedMeasBaseClass):
         rfgen.set_frequency(rf_freq0)
         return disp, rf_freq0
 
+    def correct_orbit_with_ch_rf(self, nr_iters=5):
+        """."""
+        sofb = self.devices['sofb']
+        for _ in range(nr_iters):
+            sofb.wait_buffer()
+            sofb.cmd_calccorr()
+            sofb.cmd_applycorr_ch()
+            sofb.cmd_applycorr_rf()
+            sofb.wait_apply_delta_kick()
+            sofb.cmd_reset()
+
     def _meas_func(self):
+        prms = self.params
+        qs_method = CorrectVerticalDispersionParams.CORR_METHOD.QS
+        cv_method = CorrectVerticalDispersionParams.CORR_METHOD.CV
+
         data = {}
         data['timestamp'] = _time.time()
         data['dispersion_history'] = []
 
         # CORR WITH QS
-        data['dksl_history'] = []
-        data['ksl_history'] = []
-        data['initial_ksl'] = self.get_skew_strengths()
-
-        # CORR WITH CV
-        data['dvk_history'] = []
-        data['vk_history'] = []
-        data['initial_vk'] = self.get_vertical_kicks()
+        if prms.corr_method == qs_method:
+            data['dksl_history'] = []
+            data['ksl_history'] = []
+            data['initial_ksl'] = self.get_strengths()
+        elif prms.corr_method == cv_method:
+            # CORR WITH CV
+            data['dkickcv_history'] = []
+            data['kickcv_history'] = []
+            data['initial_kickcv'] = self.get_strengths()
+            data['initial_orbit'] = self.get_orb()
+            data['orbit_history'] = []
 
         data['rf_frequency'] = []
         data['tunex'] = []
         data['tuney'] = []
 
-        # QS correction
-        if self.params.corr_method == \
-            CorrectVerticalDispersionParams.CORR_METHOD.QS:
-            if self.model_ks_dispmat is None:
-                all_qsidx_model = _np.array(self.fam_data['QS']['index']).ravel()
-                subset_qsidx = all_qsidx_model[self.params.qs2use_idx]
-                self.model_ks_dispmat = self.calc_ks_dispmat(
-                    self.model, qsidx=subset_qsidx)[160:, :]
-            idmat = self.invert_dispmat(self.model_ks_dispmat, self.params.nr_svals)
-            for idx in range(self.params.nr_iters_corr):
-                if self._stopevt.is_set():
-                    print('Stop!')
-                    break
-                disp, rffreq = self.measure_dispersion()
-                dispy = disp[160:]
-                data['dispersion_history'].append(disp)
-                data['rf_frequency'].append(rffreq)
-                data['tunex'].append(self.devices['tune'].tunex)
-                data['tuney'].append(self.devices['tune'].tuney)
-                dstrens = self.calc_ks_correction(dispy, idmat)
-                stg = f'Iter. {idx+1:02d}/{self.params.nr_iters_corr:02d} | '
-                stg += f'dispy rms: {_np.std(dispy)*1e6:.2f} [um], '
+        if self.dispmat is None:
+            raise Exception('self.dispmat is None')
+
+        idmat = self.invert_dispmat(
+            self.dispmat, prms.nr_svals)
+
+        for idx in range(prms.nr_iters_corr):
+            if self._stopevt.is_set():
+                print('Stop!')
+                break
+            disp, rffreq = self.measure_dispersion()
+            dispy = disp[160:]
+            data['dispersion_history'].append(disp)
+            data['rf_frequency'].append(rffreq)
+            data['tunex'].append(self.devices['tune'].tunex)
+            data['tuney'].append(self.devices['tune'].tuney)
+            dstrens = - idmat @ dispy
+            stg = f'Iter. {idx+1:02d}/{prms.nr_iters_corr:02d} | '
+            stg += f'dispy rms: {_np.std(dispy)*1e6:.2f} [um], '
+            if prms.corr_method == qs_method:
                 stg += f'dKsL rms: {_np.std(dstrens)*1e3:.4f} [1/km] \n'
-                print(stg)
-                print('-'*50)
                 data['dksl_history'].append(dstrens)
-                data['ksl_history'].append(self.get_skew_strengths())
-                self.apply_skew_delta_strengths(
-                    dstrens, factor=self.params.ks_amp_factor2apply)
-                self.devices['sofb'].correct_orbit_manually(nr_iters=10, residue=5)
+                data['ksl_history'].append(self.get_strengths())
+            elif prms.corr_method == cv_method:
+                stg += f'dkickV rms: {_np.std(dstrens)*1e6:.4f} [urad] \n'
+                data['dkickcv_history'].append(dstrens)
+                data['kickcv_history'].append(self.get_strengths())
+                data['orbit_history'].append(self.get_orb())
+                dstrens *= 1e6  # [rad] -> [urad]
+            print(stg)
+            print('-'*50)
+            self.apply_delta_strengths(
+                dstrens, factor=prms.amp_factor2apply)
+            if prms.corr_method == qs_method:
+                self.devices['sofb'].correct_orbit_manually(
+                    nr_iters=10, residue=5)
+            else:
+                self.correct_orbit_with_ch_rf()
 
-            dispf, rffreq = self.measure_dispersion()
-            data['final_dispersion'] = dispf
-            data['final_rf_frequency'] = rffreq
-            data['final_ksl'] = self.get_skew_strengths()
+        dispf, rffreq = self.measure_dispersion()
+        data['final_dispersion'] = dispf
+        data['final_rf_frequency'] = rffreq
+        if prms.corr_method == qs_method:
+            data['final_ksl'] = self.get_strengths()
+        elif prms.corr_method == cv_method:
+            data['final_kickcv'] = self.get_strengths()
+            data['final_orbit'] = self.get_orb()
 
-            print('='*50)
-            print('Correction result:')
-            dispy0, dispyf = data['dispersion_history'][0][160:], dispf[160:]
+        print('='*50)
+        print('Correction result:')
+        dispy0, dispyf = data['dispersion_history'][0][160:], dispf[160:]
+        stg = f'dispy rms {_np.std(dispy0)*1e6:.2f} [um] '
+        stg += f'--> {_np.std(dispyf)*1e6:.2f} [um] \n'
+        if prms.corr_method == qs_method:
             ksl0, kslf = data['initial_ksl'], data['final_ksl']
-            stg = f'dispy rms {_np.std(dispy0)*1e6:.2f} [um] '
-            stg += f'--> {_np.std(dispyf)*1e6:.2f} [um] \n'
             stg += f"KsL rms: {_np.std(ksl0)*1e3:.4f} [1/km] "
             stg += f'--> {_np.std(kslf)*1e3:.4f} [1/km] \n'
-            print(stg)
-            print('='*50)
-            print('Finished!')
-            self.data = data
+        elif prms.corr_method == cv_method:
+            kick0, kickf = data['initial_kickcv'], data['final_kickcv']
+            stg += f"kickCV rms: {_np.std(kick0):.4f} [urad] "
+            stg += f'--> {_np.std(kickf):.4f} [urad] \n'
 
-        # CV correction
-        elif self.params.corr_method == \
-            CorrectVerticalDispersionParams.CORR_METHOD.CV: # in develop
-                imat = self.invert_dispmat(self.model_vk_dispmat, self.params.nr_svals)
-                for idx in range(self.params.nr_iters_corr):
-                    if self._stopevt.is_set():
-                        print('Stop!')
-                        break
-                    disp, rffreq = self.measure_dispersion()
-                    dispy = disp[160:]
-                    data['dispersion_history'].append(disp)
-                    data['rf_frequency'].append(rffreq)
-                    data['tunex'].append(self.devices['tune'].tunex)
-                    data['tuney'].append(self.devices['tune'].tuney)
-                    dvk = self.calc_cv_correction(dispy, imat)
-                    stg = f'Iter. {idx+1:02d}/{self.params.nr_iters_corr:02d} | '
-                    stg += f'dispy rms: {_np.std(dispy)*1e6:.2f} [um], '
-                    stg += f'delta Vkicks rms: {_np.std(dvk)*1e3:.4f} [1/km] \n'
-                    print(stg)
-                    print('-'*50)
-                    data['dvk_history'].append(dvk)
-                    data['vk_history'].append(self.get_vertical_kicks())
-
-                    self.devices['sofb'].deltakickcv = dvk # sofb setter to CV delta vkicks
-
-                    # i dont know the limits of operating the sofb with manual SP of delta kicks
-
-                    ### to correct the horizontal orbit,
-                    ### must restrict the sofb manual correction to only CH:
-
-                    #self.devices['sofb'].correct_orbit_manually(nr_iters=10, residue=5) # i didnt mod yet
-
-                dispf, rffreqf = self.measure_dispersion()
-                data['final_dispersion'] = dispf
-                data['final_rf_frequency'] = rffreqf
-                data['final_vk'] = self.get_vertical_kicks()
-
-                print('='*50)
-                print('Correction result:')
-                dispy0, dispyf = data['dispersion_history'][0][160:], dispf[160:]
-                vk0, vkf = data['initial_vk'], data['final_vk']
-                stg = f'dispy rms {_np.std(dispy0)*1e6:.2f} [um] '
-                stg += f'--> {_np.std(dispyf)*1e6:.2f} [um] \n'
-                stg += f"KsL rms: {_np.std(vk0)*1e3:.4f} [1/km] "
-                stg += f'--> {_np.std(vkf)*1e3:.4f} [1/km] \n'
-                print(stg)
-                print('='*50)
-                print('Finished!')
-                self.data = data
+        print(stg)
+        print('='*50)
+        print('Finished!')
+        self.data = data
 
     def calc_model_dispersion(self, model=None):
         """."""
@@ -304,22 +293,6 @@ class CorrectVerticalDispersion(_ThreadedMeasBaseClass):
         inv_dispmat = vhmat.T @ _np.diag(ismat) @ umat.T
         return inv_dispmat
 
-    def calc_ks_correction(self, dispy, inv_dispmat=None, nr_svals=None):
-        """."""
-        if inv_dispmat is None:
-            dispmat = self.calc_ks_dispmat(self.model)
-            inv_dispmat = self.invert_dispmat(dispmat, nr_svals)
-        dstren = - inv_dispmat @ dispy
-        return dstren
-
-    def calc_cv_correction(self, dispy, inv_dispmat=None, nr_svals=None):
-        """."""
-        if inv_dispmat is None:
-            dispmat = self.model_vk_dispmat
-            inv_dispmat = self.invert_dispmat(dispmat, nr_svals)
-        dvk = (-1) * _np.dot(inv_dispmat, dispy)
-        return dvk
-
     def measure_ks_dispmat(self, dksl=1e-4):
         """."""
         meas_ks_dispmat = []
@@ -339,38 +312,3 @@ class CorrectVerticalDispersion(_ThreadedMeasBaseClass):
         print('Finished!')
         self.meas_ks_dispmat = meas_ks_dispmat
         return meas_ks_dispmat
-
-    @staticmethod
-    def calc_disp_respmat(model, delta_energy=2e-6):
-        """Calculate model dispy x corrs respmat."""
-        def calc_vdisp(model, indices):
-            disp = calc_disp(model=model, indices=indices)
-            return disp[int(len(disp)/2):]
-
-        def calc_disp(model, indices, delta_energy):
-            orbp = pyaccel.tracking.find_orbit4(
-                model, indices=indices, energy_offset=+delta_energy/2)
-            orbn = pyaccel.tracking.find_orbit4(
-                model, indices=indices, energy_offset=-delta_energy/2)
-            dispx = (orbp[0, :] - orbn[0, :])/delta_energy
-            dispy = (orbp[2, :] - orbn[2, :])/delta_energy
-            return _np.hstack([dispx, dispy])
-
-        def getline(model, idx, delta_energy, bpmidx):
-            v0 = model[idx].vkick_polynom
-            model[idx].vkick_polynom = v0 + delta_energy/2
-            dispp = calc_vdisp(model, 'open')[bpmidx]
-            model[idx].vkick_polynom = v0 - delta_energy/2
-            dispn = calc_vdisp(model, 'open')[bpmidx]
-            model[idx].vkick_polynom = v0
-            disp = (dispp - dispn)/delta_energy
-            return disp
-
-        famdata = si.get_family_data(model)
-        cvidx = _np.array(famdata['CV']['index']).ravel()
-        bpmidx = _np.array(famdata['BPM']['index']).ravel()
-        lines = []
-        for i in cvidx:
-            lines.append(getline(model, i, delta_energy, bpmidx))
-        lines = _np.array(lines).T
-        return lines
