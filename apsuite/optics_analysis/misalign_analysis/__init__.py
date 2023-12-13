9e3fd55fa181ed3bfe83bbf40a423f0a42df8176
VitorSouzaLNLS <vitor.souza@lnls.br>
2023-12-13 10:59:39 -0300

    moving

---
+++ apsuite/optics_analysis/misalign_analysis/__init__.py
@@ -0,0 +1,10 @@
+"""Pynel package."""
+
+from . import std_si_data
+from . import functions as functions
+from .buttons import Button
+from .base import Base
+# from . import fitting
+
+__all__ = ['Base', 'Button', 'functions', 'std_si_data']
+# __all__ = ['Base', 'Button', 'functions', 'fitting', 'std_si_data']
--- apsuite/pynel/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Pynel package."""
-
-from . import std_si_data
-from . import functions as functions
-from .buttons import Button
-from .base import Base
-from . import fitting
-
-__all__ = ['Base', 'Button', 'functions', 'fitting', 'std_si_data']
