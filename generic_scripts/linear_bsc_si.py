b288898870c7231f8a8a7bce743352d49aa08ff9
Otavio Jose de Rezende Silveira <otavio.silveira719@gmail.com>
2024-08-15 14:19:37 -0300

    add analyses of BSC at straight sections

--- generic_scripts/linear_bsc_si.py
+++ generic_scripts/linear_bsc_si.py
@@ -4,6 +4,7 @@ BSC was calculated considering linear approximation.
 """
 
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import numpy as np
 import pyaccel as pa
 from pymodels import si
@@ -108,3 +109,88 @@ axs[1].axis("off")
 axs[0].set_xlim(0, model.length / symmetry)
 
 fig.savefig("si_bsc.svg")
+
+# calculates BSC for Sirius straight sections
+
+
+def beta_ss(spos, beta0):
+    """Calculates beta at the center of a straight section."""
+    return beta0 + spos * spos / beta0
+
+
+def bsc_ss(beta, beta0, bsc0):
+    """Calculates BSC at the center of a straight section."""
+    return np.sqrt(beta / beta0) * bsc0
+
+
+symmetry = 20
+
+# center points of the first 3 straight sections (SA, SB, SP)
+center_ss = model.length / symmetry * np.arange(3)
+dist_center = np.linspace(0, 3, 100)
+
+bscx_ss = []
+bscy_ss = []
+
+for pos in center_ss:
+    idx = np.searchsorted(spos, pos)
+    betax0 = tws.betax[idx]
+    betay0 = tws.betay[idx]
+    bscx0 = bscx[idx]
+    bscy0 = bscy[idx]
+
+    betax = beta_ss(dist_center, betax0)
+    betay = beta_ss(dist_center, betay0)
+
+    bscx_ss.append(bsc_ss(betax, betax0, bscx0))
+    bscy_ss.append(bsc_ss(betay, betay0, bscy0))
+
+bscx_a = bscx_ss[0]  # BSCx at center of high-beta straight section SA
+bscx_b = bscx_ss[1]  # BSCx at center of low-beta straight section SB
+bscx_p = bscx_ss[2]  # BSCx at center of low-beta straight section SP
+
+bscy_a = bscy_ss[0]  # BSCy at center of high-beta straight section SA
+bscy_b = bscy_ss[1]  # BSCy at center of low-beta straight section SB
+bscy_p = bscy_ss[2]  # BSCy at center of low-beta straight section SP
+
+# plots the BSC figure at straight sections for wiki-sirius
+res = 512 / 386
+h = 6
+
+fig, axs = plt.subplots(
+    2,
+    2,
+    figsize=(res * h, h),
+    sharex=True,
+    gridspec_kw=dict(
+        left=0.12, right=0.96, top=0.95, bottom=0.08, hspace=0.1, wspace=0.14
+    )
+)
+
+axs = axs.flat
+
+axs[0].plot(dist_center, bscx_a, color="blue")
+axs[0].set_ylabel("Horizontal BSC [mm]")
+axs[0].set_title("High Beta SS (SA)")
+
+axs[1].plot(dist_center, bscx_b, color="blue")
+axs[1].plot(dist_center, bscx_p, color="blue")
+axs[1].set_title("Low Beta SS (SB, SP)")
+
+axs[2].plot(dist_center, bscy_a, color="red")
+axs[2].set_xlabel("distance from center [m]")
+axs[2].set_ylabel("Vertical BSC [mm]")
+
+axs[3].plot(dist_center, bscy_b, color="red")
+axs[3].plot(dist_center, bscy_p, color="red")
+axs[3].set_xlabel("distance from center [m]")
+
+for ax in axs:
+    ax.minorticks_on()
+    ax.grid(which="minor", linestyle=":")
+    ax.yaxis.set_major_locator(ticker.MaxNLocator(7))
+    ax.set_xlim(dist_center[0], dist_center[-1])
+    ticks = ax.get_yticks()
+    ax.set_ylim(ticks[0], ticks[-1])
+
+fig.savefig("si_bsc_straight_sec.svg")
