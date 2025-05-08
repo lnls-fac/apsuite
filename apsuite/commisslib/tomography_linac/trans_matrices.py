171165824e16e67d00a2dca366d075fbcc0e6896
Fernando <fernandohds564@gmail.com>
2025-05-08 11:07:15 -0300

    COMMISS.MENT.ENH: Update matrix from QF3 to Screen.

--- apsuite/commisslib/tomography_linac/trans_matrices.py
+++ apsuite/commisslib/tomography_linac/trans_matrices.py
@@ -19,15 +19,21 @@ def matrix_quad(kl):
     return _np.array([[1, 0], [-kl, 1]])
 
 
-def matrix_to_scrn(kl):
-    """Transfer matrix from the end of QF2-2 to the screen in the LINAC.
+def matrix_from_qf3_to_scrn(kl):
+    """Transfer matrix from the start of QF3 to Scren-05 in the LINAC.
 
     Args:
     kl (float): Integrated quadrupole strength.
 
     Returns:
-    array: Transfer matrix as a NumPy array.
+        array: Transfer matrix as a NumPy array.
     """
-    l1 = 0.1
+    # this value was taken from the emittance measurement script and matches
+    # the .dwg drawing:
     l2 = 2.8775
-    return matrix_drift(l2) @ matrix_quad(kl) @ matrix_drift(l1)
+    # This is the magnet length provided by the LINAC technical design report.
+    # It is also being used by the emittance measurement script. However,
+    # There is still doubt whether this is the physical or the effective
+    # length of the magnet
+    quad_len = 0.112
+    return matrix_drift(l2) @ matrix_quad(kl) @ matrix_drift(quad_len/2)
