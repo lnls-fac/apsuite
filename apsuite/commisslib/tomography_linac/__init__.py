0e278b8b75fa29281d913f4e7caa44b2253b77a4
Otavio Jose de Rezende Silveira <otavio.silveira719@gmail.com>
2025-07-04 16:42:46 -0300

    correct import

--- apsuite/commisslib/tomography_linac/__init__.py
+++ apsuite/commisslib/tomography_linac/__init__.py
@@ -3,6 +3,6 @@
 from .ment_reconstruction import DistribReconstruction, paint_convex, \
     plot_convex, plot_lines, ScreenProcess
 from .trans_matrices import matrix_drift, matrix_quad, matrix_rotation, \
-    matrix_to_scrn
+    matrix_from_qf3_to_scrn
 
 del trans_matrices, ment_reconstruction
