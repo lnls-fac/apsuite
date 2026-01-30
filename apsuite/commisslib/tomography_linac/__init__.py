3b93d0a22a84a1d66e15d306829cf9ead17c7ca0
Otavio Jose de Rezende Silveira <otavio.silveira719@gmail.com>
2026-01-30 16:01:48 -0300

    COMMISS.TOMOG.API: add new class in __init__.py

--- apsuite/commisslib/tomography_linac/__init__.py
+++ apsuite/commisslib/tomography_linac/__init__.py
@@ -4,5 +4,6 @@ from .ment_reconstruction import DistribReconstruction, paint_convex, \
     plot_convex, plot_lines, ScreenProcess
 from .trans_matrices import matrix_drift, matrix_quad, matrix_rotation, \
     matrix_from_qf3_to_scrn
+from .measure_tomography import MeasTomography
 
-del trans_matrices, ment_reconstruction
+del trans_matrices, ment_reconstruction, measure_tomography
