a9e5974db1c1e498eb84117138b73b6f37dd35f7
Matheus <vellosok75@gmail.com>
2026-03-04 10:23:18 -0300

    COMMISS.SPIN.MEAS: refactor measurement
    - simplify flow by abandoning use of several threads
    - previous scheme used a thread to control bbbv frequency steps and a thread for continuously acquire data upon
    - now, single measurement thread is  launched: scan frequencies, wait depol, acq data and proceed

--- apsuite/commisslib/measure_spin_depol_scan.py
+++ apsuite/commisslib/measure_spin_depol_scan.py
@@ -1,15 +1,14 @@
 """."""
 
 import time as _time
-import numpy as _np
-import matplotlib.pyplot as _mplt
-from threading import Event as _Event, Thread as _Thread
 
-from .. utils import ThreadedMeasBaseClass, ParamsBaseClass
+import mathphys.constants as _const
+import matplotlib.pyplot as _mplt
+import numpy as _np
 from siriuspy.devices import CurrInfoSI, RFGen, Tune
 from siriuspy.devices.bbb import BunchbyBunch
 
-import mathphys.constants as _const
+from ..utils import ParamsBaseClass, ThreadedMeasBaseClass
 
 A_ELECTRON = 1.15965218059e-3
 E_electron = _const.electron_rest_energy / _const.elementary_charge  # [eV]
@@ -21,22 +20,21 @@ class MeasureSpinDepolScanParams(ParamsBaseClass):
     def __init__(self):
         """."""
         super().__init__()
-        self.freq_begin = 400  # [kHz]
-        self.freq_end = 500  # [kHz]
-        self.freq_step = 1  # [kHz]
-        self.time_base_update_time = 1  # [s]
-        self.excitation_time = 1  # [s]
-        self.update_timeout = 3  # [s]
+        self.freq_start = 400.  # [kHz]
+        self.freq_stop = 500.  # [kHz]
+        self.freq_step = 1.  # [kHz]
+        self.time_base_update_time = 1.  # [s]
+        self.excitation_time = 1.  # [s]
 
     def __str__(self):
         """."""
         ftmp = '{0:24s} = {1:9.3f}  {2:s}\n'.format
-        dtmp = '{0:24s} = {1:9d}  {2:s}\n'.format
-        stmp = '{0:24s} = {1:9s}  {2:s}\n'.format
+        # dtmp = '{0:24s} = {1:9d}  {2:s}\n'.format
+        # stmp = '{0:24s} = {1:9s}  {2:s}\n'.format
 
         stg = ''
-        stg += ftmp('freq_begin', self.freq_begin, '[kHz]')
-        stg += ftmp('freq_end', self.freq_end, '[kHz]')
+        stg += ftmp('freq_start', self.freq_start, '[kHz]')
+        stg += ftmp('freq_stop', self.freq_stop, '[kHz]')
         stg += ftmp('freq_step', self.freq_step, '[kHz]')
         stg += ftmp('time_base_update_time', self.time_base_update_time, '[s]')
         stg += ftmp('excitation_time', self.excitation_time, '[s]')
@@ -46,9 +44,12 @@ class MeasureSpinDepolScanParams(ParamsBaseClass):
 class MeasureSpinDepolScan(ThreadedMeasBaseClass):
     """."""
 
-    def __init__(self, params=MeasureSpinDepolScanParams(), isonline=True):
-        super().__init__(params, isonline)
-        self.target = self.do_measurement
+    def __init__(self, isonline=True):
+        super().__init__(
+            params=MeasureSpinDepolScanParams(),
+            target=self.do_measurement,
+            isonline=isonline,
+        )
         if self.isonline:
             self.create_devices()
 
@@ -61,104 +62,80 @@ class MeasureSpinDepolScan(ThreadedMeasBaseClass):
         self.devices['gamma_counters'] = None
         self.devices['blm_counters'] = None
 
+        self.pvs['bbbv_freq_sp'] = self.devices['bbbv'].pv_object(
+            'SI-Glob:DI-BbBProc-V:DRIVE1_FREQ'
+        )
+        self.pvs['bbbv_freq_rb'] = self.devices['bbbv'].pv_object(
+            'SI-Glob:DI-BbBProc-V:DRIVE1_FREQ_ACT_STRING'
+        )
+
     def get_data(self):
         """."""
-        flag = _Event()
 
-        def update(*args, **kwargs):
-            """."""
-            flag.set()
+        self.data['timestamps'].append(_time.time())
+        self.data['lifetime'].append(self.devices['currinfo'].lifetime)
+        self.data['stored_current'].append(self.devices['currinfo'].current)
+        self.data['bbbv_freq_sp'].append(self.pvs['bbbv_freq_sp'].value)
+        self.data['bbbv_freq_rb'].append(self.pvs['bbbv_freq_rb'].value)
+        self.data['rf_freq'].append(self.devices['rfgen'].frequency)
+        self.data['tunex'].append(self.devices['tune'].tunex)
+        self.data['tuney'].append(self.devices['tune'].tuney)
+        self.data['gamma_counters'].append(self.get_gamma_data())
+        self.data['blm_counters'].append(self.get_blm_data())
 
-        curr_mon = self.devices['currinfo'].pv_object('Current-Mon')
-        curr_mon.auto_monitor = True
-        curr_mon.add_callback(update)
-
-        freq_sp = self.devices['bbbv'].pv_object(
-            'SI-Glob:DI-BbBProc-V:DRIVE0_FREQ'
-        )
-        freq_rb = self.devices['bbbv'].pv_object(
-            'SI-Glob:DI-BbBProc-V:DRIVE0_FREQ_ACT_STRING'
-        )
-        while not self._stopevt.is_set():
-
-            if flag.wait(self.params.update_timeout):
-                self.data['timestamps'].append(_time.time())
-                self.data['lifetime'].append(self.devices['currinfo'].lifetime)
-                self.data['stored_current'].append(
-                    self.devices['currinfo'].current
-                )
-                self.data['bbbv_frequency_sp'].append(freq_sp.value)
-                self.data['bbbv_frequency_rb'].append(freq_rb.value)
-                self.data['rf_frequency'].append(
-                    self.devices['rfgen'].frequency
-                )
-                self.data['tunex'].append(self.devices['tune'].tunex)
-                self.data['tuney'].append(self.devices['tune'].tuney)
-                self.data['gamma_counters'].append(self.get_gamma_data())
-                self.data['blm_counters'].append(self.get_blm_data())
-                flag.clear()
-            else:
-                print('Warning: timeout waiting for current update!')
-                break
-
-        curr_mon.clear_callbacks(update)
-        curr_mon.auto_monitor = False
-
-        for k, v in self.data.items():
-            self.data[k] = _np.array(v)
-
-    def get_gamma_data(self,):
+    def get_gamma_data(self):
         """."""
         raise NotImplementedError
 
-    def get_blm_data(self,):
+    def get_blm_data(self):
         """."""
         raise NotImplementedError
-
-    def do_scan(self,):
-        """."""
-
-        beg = self.params.freq_begin
-        end = self.params.freq_end
-        step = self.params.freq_step
-        freq_span = _np.linspace(
-            start=beg, stop=end, num=int((end - beg) / step) + 1
-        )
-        for i, freq in enumerate(freq_span):
-            if self._stopevt.is_set():
-                break
-            self.devices['bbbv'].drive0.frequency = freq
-            _time.sleep(self.params.excitation_time)
-            print(
-                f'Driving freq. {freq:.3f} kHz ({i+1})/{freq_span.size:03d})'
-            )
-        print('Scan finished.')
-        self.stop()
-
 
     def do_measurement(self):
         """."""
+        self._freq_initial = self.devices['bbbv'].drive1.frequency
+
         self.data = {
             'timestamps': [],
             'lifetime': [],
             'stored_current': [],
-            'bbbv_frequency_sp': [],
-            'bbbv_frequency_rb': [],
-            'rf_frequency': [],
+            'bbbv_freq_sp': [],
+            'bbbv_freq_rb': [],
+            'rf_freq': [],
             'tunex': [],
             'tuney': [],
             'gamma_counters': [],
             'blm_counters': [],
-            }
+        }
 
-        acq_thread = _Thread(target=self.get_data, daemon=True)
-        scan_thread = _Thread(target=self.do_scan, daemon=True)
+        start = self.params.freq_start
+        stop = self.params.freq_stop
+        step = self.params.freq_step
+        freq_span = _np.arange(
+            start=start,
+            stop=stop+step,
+            step=step,
+            dtype=float,
+        )
 
-        acq_thread.start()
-        scan_thread.start()
+        try:
+            for i, freq in enumerate(freq_span):
+                if self._stopevt.is_set():
+                    print('Stopped by user. Restoring initial DRIVE freq.')
+                    break
+                self.devices['bbbv'].drive1.frequency = freq
+                print(
+                    f'Driving freq. {freq:.3f} kHz' +
+                    f' ({i + 1})/{freq_span.size:03d})'
+                )
+                _time.sleep(self.params.excitation_time)
+                self.get_data()
+            print('Scan finished.')
+        finally:
+            self.devices['bbbv'].drive1.frequency = self._freq_initial
 
-        scan_thread.join()
-        acq_thread.join()
+        for k, v in self.data.items():
+            self.data[k] = _np.array(v)
 
 
 class SpinDepolUtils:
