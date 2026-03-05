cce426d5ccc2d78008f81aa47404a739b2c7b95e
Fernando <fernandohds564@gmail.com>
2026-03-05 16:59:26 -0300

    ENH: (COMMISS.SPIN) Improve measurement class.

--- apsuite/commisslib/measure_spin_depol_scan.py
+++ apsuite/commisslib/measure_spin_depol_scan.py
@@ -4,13 +4,54 @@ import time as _time
 
 import mathphys.constants as _const
 import numpy as _np
-from siriuspy.devices import CurrInfoSI, RFGen, Tune
-from siriuspy.devices.bbb import BunchbyBunch
+from siriuspy.devices import (
+    BunchbyBunch,
+    CurrInfoSI,
+    FamBLMs,
+    FamGammaMonitors,
+    RFGen,
+    Tune
+)
 
 from ..utils import ParamsBaseClass, ThreadedMeasBaseClass
 
-A_ELECTRON = 1.15965218059e-3
-E_electron = _const.electron_rest_energy / _const.elementary_charge  # [eV]
+
+class SpinDepolUtils:
+    """."""
+
+    ELECTRON_A = 1.15965218059e-3  # anomalous magnetic moment of the electron
+    ELECTRON_E = _const.electron_rest_energy / _const.elementary_charge  # [eV]
+
+    @classmethod
+    def calc_spin_tune_from_energy(cls, energy):
+        """Calculate the spin tune and its fractional parts from beam energy.
+
+        Args:
+            energy (float): Beam energy in [GeV]
+
+        Returns:
+            tuple of floats (spin_tune, spin_tune_frac, 1-spin_tune_frac)
+            containing the spin tune value, its fractional part and the
+            its complementary fractional part.
+        """
+        gamma = (energy * 1e9) / cls.ELECTRON_E
+        spin_tune = cls.ELECTRON_A * gamma
+        spin_tune_frac = spin_tune - int(spin_tune)
+        return spin_tune, spin_tune_frac, 1 - spin_tune_frac
+
+    @classmethod
+    def calc_energy_from_spin_tune(cls, spin_tune):
+        """Calculate the beam energy from the spin tune value.
+
+        Args:
+            spin_tune (float): _description_
+
+        Returns:
+            float: Beam energy in [GeV]
+        """
+        gamma = spin_tune / cls.ELECTRON_A
+        energy = gamma * cls.ELECTRON_E
+        return energy / 1e9
 
 
 class MeasureSpinDepolScanParams(ParamsBaseClass):
@@ -25,12 +66,12 @@ class MeasureSpinDepolScanParams(ParamsBaseClass):
         # Note: amplifier gain values above 38 dB results in nonlinearities in
         # the BBB output, revealed by the appearance of higher harmonics in the
         # spectrum of the output signal
-        self.bbb_drive_pattern = '2:1'  # not sure this format works
+        self.bbb_drive_pattern = '2:1'
+        self.freq_harmonic = 200  # harmonic number to be excited
         self.freq_start = 400.0  # [kHz]
         self.freq_stop = 500.0  # [kHz]
-        self.freq_step = 1.0  # [kHz]
-        self.time_base_update_time = 1.0  # [s]
-        self.excitation_time = 1.0  # [s]
+        self.freq_step = 0.001  # [kHz]
+        self.excitation_time = 1.5  # [s]
 
     def __str__(self):
         """."""
@@ -41,10 +82,10 @@ class MeasureSpinDepolScanParams(ParamsBaseClass):
         stg = ''
         stg += ftmp('bbb_amp_gain', self.bbb_amp_gain, '[dB]')
         stg += stmp('bbb_drive_pattern', self.bbb_drive_pattern, '')
+        stg += ftmp('freq_harmonic', self.freq_harmonic, '')
         stg += ftmp('freq_start', self.freq_start, '[kHz]')
         stg += ftmp('freq_stop', self.freq_stop, '[kHz]')
         stg += ftmp('freq_step', self.freq_step, '[kHz]')
-        stg += ftmp('time_base_update_time', self.time_base_update_time, '[s]')
         stg += ftmp('excitation_time', self.excitation_time, '[s]')
         return stg
 
@@ -58,65 +99,53 @@ class MeasureSpinDepolScan(ThreadedMeasBaseClass):
             target=self.do_measurement,
             isonline=isonline,
         )
+        self.depol_utils = SpinDepolUtils()
         if self.isonline:
             self.create_devices()
 
     def create_devices(self):
         """."""
-        self.devices['bbbv'] = BunchbyBunch(BunchbyBunch._devices['V'])
+        self.devices['bbbv'] = BunchbyBunch(BunchbyBunch.DEVICES.V)
+        self.devices['bbbh'] = BunchbyBunch(BunchbyBunch.DEVICES.H)
         self.devices['currinfo'] = CurrInfoSI()
         self.devices['rfgen'] = RFGen(props2init=['Frequency-Mon'])
         self.devices['tune'] = Tune(Tune.DEVICES.SI)
-        self.devices['gamma_counters'] = None
-        self.devices['blm_counters'] = None
-        # TODO: add also tune from BBB and SRAM peaks, CAX beamsizes, etc
-
-        self.pvs['bbbv_freq_sp'] = self.devices['bbbv'].drive1.pv_object(
-            'SI-Glob:DI-BbBProc-V:DRIVE1_FREQ'
-        )
-        self.pvs['bbbv_freq_rb'] = self.devices['bbbv'].drive1.pv_object(
-            'SI-Glob:DI-BbBProc-V:DRIVE1_FREQ_ACT_STRING'
-        )
+        self.devices['gamma_monitors'] = FamGammaMonitors()
+        self.devices['blms'] = FamBLMs()
+        # TODO: add also CAX beamsizes, etc
 
     def get_data(self):
         """."""
-        self.data['timestamps'].append(_time.time())
-        self.data['lifetime'].append(self.devices['currinfo'].lifetime)
-        self.data['stored_current'].append(self.devices['currinfo'].current)
-        self.data['bbbv_freq_sp'].append(self.pvs['bbbv_freq_sp'].value)
-        self.data['bbbv_freq_rb'].append(self.pvs['bbbv_freq_rb'].value)
-        self.data['rf_freq'].append(self.devices['rfgen'].frequency)
-        self.data['tunex'].append(self.devices['tune'].tunex)
-        self.data['tuney'].append(self.devices['tune'].tuney)
-        self.data['gamma_counters'].append(self.get_gamma_data())
-        self.data['blm_counters'].append(self.get_blm_data())
-
-    def get_gamma_data(self):
-        """."""
-        raise NotImplementedError
-
-    def get_blm_data(self):
-        """."""
-        raise NotImplementedError
+        data = {}
+        bbbv = self.devices['bbbv']
+        bbbh = self.devices['bbbh']
+        data['timestamp'] = _time.time()
+        data['lifetime'] = self.devices['currinfo'].lifetime
+        data['stored_current'] = self.devices['currinfo'].current
+        data['bbbv_freq_sp'] = bbbv.drive1['FREQ']
+        data['bbbv_freq_rb'] = bbbv.drive1.frequency
+        data['bbbv_tune'] = bbbv.single_bunch.spec_marker1_tune
+        data['bbbh_tune'] = bbbh.single_bunch.spec_marker1_tune
+        data['bbbv_sram_mag'] = bbbv.sram.spec_marker1_mag
+        data['bbbh_sram_mag'] = bbbh.sram.spec_marker1_mag
+        data['bbbv_sram_tune'] = bbbv.sram.spec_marker1_tune
+        data['bbbh_sram_tune'] = bbbh.sram.spec_marker1_tune
+        data['rf_freq'] = self.devices['rfgen'].frequency
+        data['tunex'] = self.devices['tune'].tunex
+        data['tuney'] = self.devices['tune'].tuney
+        data['gamma_counts'] = self.devices['gamma_monitors'].counts
+        data['blm_counts'] = self.devices['blms'].counts
+        return data
 
     def do_measurement(self):
         """."""
-        self._freq_initial = self.devices['bbbv'].drive1.frequency
-        self._gain_initial = self.devices['bbbv'].pwr_amp.gain
-        self._patt_intial = self.devices['bbbv'].drive1.mask_pattern
+        bbbv = self.devices['bbbv']
 
-        self.data = {
-            'timestamps': [],
-            'lifetime': [],
-            'stored_current': [],
-            'bbbv_freq_sp': [],
-            'bbbv_freq_rb': [],
-            'rf_freq': [],
-            'tunex': [],
-            'tuney': [],
-            'gamma_counters': [],
-            'blm_counters': [],
-        }
+        freq_initial = bbbv.drive1.frequency
+        gain_initial = bbbv.pwr_amp.gain
+        patt_intial = bbbv.drive1.mask_pattern
+
+        self.data = {}
 
         start = self.params.freq_start
         stop = self.params.freq_stop
@@ -124,64 +153,33 @@ class MeasureSpinDepolScan(ThreadedMeasBaseClass):
         freq_span = _np.arange(
             start=start, stop=stop + step, step=step, dtype=float
         )
+        harm_freq = self.params.freq_harmonic
+        harm_freq *= bbbv.info.revolution_freq_mon / 1e3
 
-        self.devices['bbbv'].pwr_amp.gain = self.params.bbb_amp_gain
-        self.devices[
-            'bbbv'
-        ].drive1.mask_pattern = self.params.bbb_drive_pattern
+        bbbv.pwr_amp.gain = self.params.bbb_amp_gain
+        bbbv.drive1.mask_pattern = self.params.bbb_drive_pattern
 
         try:
+            siz = freq_span.size
             for i, freq in enumerate(freq_span):
                 if self._stopevt.is_set():
                     print('Stopped by user. Restoring BBB initial state.')
                     break
-                self.devices['bbbv'].drive1.frequency = freq
-                print(
-                    f'Driving freq. {freq:.3f} kHz'
-                    + f' ({i + 1})/{freq_span.size:03d})'
-                )
+                bbbv.drive1.frequency = harm_freq + freq
+                print(f' {i + 1:03d}/{siz:03d} -> freq. {freq:.3f} kHz')
                 _time.sleep(self.params.excitation_time)
-                self.get_data()
+
+                data = self.get_data()
+                for k, v in data.items():
+                    if k not in self.data:
+                        self.data[k] = []
+                    self.data[k].append(v)
+
             print('Scan finished.')
         finally:
-            self.devices['bbbv'].drive1.frequency = self._freq_initial
-            self.devices['bbbv'].pwr_amp.gain = self._gain_initial
-            self.devices['bbbv'].drive1.mask_pattern = self._patt_intial
+            bbbv.drive1.frequency = freq_initial
+            bbbv.pwr_amp.gain = gain_initial
+            bbbv.drive1.mask_pattern = patt_intial
 
         for k, v in self.data.items():
             self.data[k] = _np.array(v)
-
-
-class SpinDepolUtils:
-    """."""
-
-    @staticmethod
-    def calc_spin_tune_from_energy(energy):
-        """Calculate the spin tune and its fractional parts from beam energy.
-
-        Args:
-            energy (float): Beam energy in [GeV]
-
-        Returns:
-            tuple of floats (spin_tune, spin_tune_frac, 1-spin_tune_frac)
-            containing the spin tune value, its fractional part and the
-            its complementary fractional part.
-        """
-        gamma = (energy * 1e9) / E_electron
-        spin_tune = A_ELECTRON * gamma
-        spin_tune_frac = spin_tune - int(spin_tune)
-        return spin_tune, spin_tune_frac, 1 - spin_tune_frac
-
-    @staticmethod
-    def calc_energy_from_spin_tune(spin_tune):
-        """Calculate the beam energy from the spin tune value.
-
-        Args:
-            spin_tune (float): _description_
-
-        Returns:
-            float: Beam energy in [GeV]
-        """
-        gamma = spin_tune / A_ELECTRON
-        energy = gamma * E_electron
-        return energy / 1e9
