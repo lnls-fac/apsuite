35d7718cb58023f9c83d33fe1ad1de174d621a66
Otavio Silveira <otavio.silveira719@gmail.com>
2026-06-08 07:44:00 -0300

    COMMISS.RESPMAT.TBBO: update to ruff standarts

--- apsuite/commisslib/measure_respmat_tbbo.py
+++ apsuite/commisslib/measure_respmat_tbbo.py
@@ -1,5 +1,6 @@
 """."""
 
+import time as _time
 import numpy as np
 
 import pyaccel
@@ -7,8 +8,10 @@ from siriuspy.namesys import SiriusPVName as _PVName
 from siriuspy.devices import SOFB
 
 from ..optimization import SimulAnneal
-from ..utils import ThreadedMeasBaseClass as _BaseClass, \
-    ParamsBaseClass as _ParamsBaseClass
+from ..utils import (
+    ThreadedMeasBaseClass as _BaseClass,
+    ParamsBaseClass as _ParamsBaseClass,
+)
 
 
 class Params(_ParamsBaseClass):
@@ -38,9 +41,9 @@ class MeasureRespMatTBBO(_BaseClass):
         """."""
         super().__init__(params=Params(), target=self._measure_matrix_thread)
         self.devices = {
-            'bo_sofb': SOFB(SOFB.DEVICES.BO),
-            'tb_sofb': SOFB(SOFB.DEVICES.TB),
-            }
+            "bo_sofb": SOFB(SOFB.DEVICES.BO),
+            "tb_sofb": SOFB(SOFB.DEVICES.TB),
+        }
         self._all_corrs = all_corrs
         self._matrix = dict()
         self._corrs_to_measure = []
@@ -49,25 +52,27 @@ class MeasureRespMatTBBO(_BaseClass):
     def trajx(self):
         """."""
         return np.hstack(
-            [self.devices['tb_sofb'].trajx, self.devices['bo_sofb'].trajx])
+            [self.devices["tb_sofb"].trajx, self.devices["bo_sofb"].trajx]
+        )
 
     @property
     def trajy(self):
         """."""
         return np.hstack(
-            [self.devices['tb_sofb'].trajy, self.devices['bo_sofb'].trajy])
+            [self.devices["tb_sofb"].trajy, self.devices["bo_sofb"].trajy]
+        )
 
     def wait(self, timeout=10):
         """."""
-        self.devices['tb_sofb'].wait_buffer(timeout=timeout)
-        self.devices['bo_sofb'].wait_buffer(timeout=timeout)
+        self.devices["tb_sofb"].wait_buffer(timeout=timeout)
+        self.devices["bo_sofb"].wait_buffer(timeout=timeout)
 
     def reset(self, wait=0):
         """."""
         if self._stopevt.wait(wait):
             return False
-        self.devices['tb_sofb'].cmd_reset()
-        self.devices['bo_sofb'].cmd_reset()
+        self.devices["tb_sofb"].cmd_reset()
+        self.devices["bo_sofb"].cmd_reset()
         if self._stopevt.wait(1):
             return False
         return True
@@ -75,10 +80,12 @@ class MeasureRespMatTBBO(_BaseClass):
     @property
     def corr_names(self):
         """."""
-        corrs = sorted([
-            c for c in self._all_corrs if not c.dev.startswith('CV')])
-        corrs.extend(sorted([
-            c for c in self._all_corrs if c.dev.startswith('CV')]))
+        corrs = sorted(
+            [c for c in self._all_corrs if not c.dev.startswith("CV")]
+        )
+        corrs.extend(
+            sorted([c for c in self._all_corrs if c.dev.startswith("CV")])
+        )
         return corrs
 
     @property
@@ -228,65 +235,85 @@ class MeasureRespMatTBBO(_BaseClass):
 
 
 def calc_model_respmatTBBO(
-        tb_mod, model, corr_names, elems, meth='middle', ishor=True):
+    tb_mod, model, corr_names, elems, meth="middle", ishor=True
+):
     """."""
-    bpms = np.array(pyaccel.lattice.find_indices(model, 'fam_name', 'BPM'))[1:]
+    bpms = np.array(pyaccel.lattice.find_indices(model, "fam_name", "BPM"))[1:]
     _, cumulmat = pyaccel.tracking.find_m44(
-        model, indices='open', fixed_point=[0, 0, 0, 0])
+        model, indices="open", fixed_point=[0, 0, 0, 0]
+    )
 
-    matrix = np.zeros((len(corr_names), 2*bpms.size))
+    matrix = np.zeros((len(corr_names), 2 * bpms.size))
     for idx, corr in enumerate(corr_names):
         elem = elems[corr]
         indcs = np.array(elem.model_indices)
-        if corr.sec == 'BO':
-            print('Booster ', corr)
+        if corr.sec == "BO":
+            print("Booster ", corr)
             indcs += len(tb_mod)
         cortype = elem.magnet_type
         kxl = kyl = ksxl = ksyl = 0
-        if corr.dev == 'InjSept':
+        if corr.dev == "InjSept":
             # kxl = tb_mod[indcs[0][1]].KxL
             # kyl = tb_mod[indcs[0][1]].KyL
             # ksxl = tb_mod[indcs[0][1]].KsxL
             # ksyl = tb_mod[indcs[0][1]].KsyL
             midx = pyaccel.lattice.find_indices(
-                tb_mod, 'fam_name', 'InjSeptM66')
+                tb_mod, "fam_name", "InjSeptM66"
+            )
             for m in midx:
                 kxl += tb_mod[m].KxL
                 kyl += tb_mod[m].KyL
                 ksxl += tb_mod[m].KsxL
                 ksyl += tb_mod[m].KsyL
-        if not ishor and corr.dev in {'InjSept', 'InjKckr'}:
-            cortype = 'vertical'
+        if not ishor and corr.dev in {"InjSept", "InjKckr"}:
+            cortype = "vertical"
         matrix[idx, :] = _get_respmat_line(
-            cumulmat, indcs, bpms, length=elem.model_length,
-            kxl=kxl, kyl=kyl, ksxl=ksxl, ksyl=ksyl,
-            cortype=cortype, meth=meth)
+            cumulmat,
+            indcs,
+            bpms,
+            length=elem.model_length,
+            kxl=kxl,
+            kyl=kyl,
+            ksxl=ksxl,
+            ksyl=ksyl,
+            cortype=cortype,
+            meth=meth,
+        )
     return matrix
 
 
 def _get_respmat_line(
-        cumul_mat, indcs, bpms, length, kxl=0, kyl=0, ksxl=0, ksyl=0,
-        cortype='vertical', meth='middle'):
+    cumul_mat,
+    indcs,
+    bpms,
+    length,
+    kxl=0,
+    kyl=0,
+    ksxl=0,
+    ksyl=0,
+    cortype="vertical",
+    meth="middle",
+):
 
-    idx = 3 if cortype.startswith('vertical') else 1
+    idx = 3 if cortype.startswith("vertical") else 1
     cor = indcs[0]
-    if meth.lower().startswith('end'):
-        cor = indcs[-1]+1
-    elif meth.lower().startswith('mid'):
+    if meth.lower().startswith("end"):
+        cor = indcs[-1] + 1
+    elif meth.lower().startswith("mid"):
         # create a symplectic integrator of second order
         # for the last half of the element:
         drift = np.eye(4, dtype=float)
-        drift[0, 1] = length/2 / 2
-        drift[2, 3] = length/2 / 2
+        drift[0, 1] = length / 2 / 2
+        drift[2, 3] = length / 2 / 2
         quad = np.eye(4, dtype=float)
-        quad[1, 0] = -kxl/2
-        quad[3, 2] = -kyl/2
-        quad[1, 2] = -ksxl/2
-        quad[3, 0] = -ksyl/2
+        quad[1, 0] = -kxl / 2
+        quad[3, 2] = -kyl / 2
+        quad[1, 2] = -ksxl / 2
+        quad[3, 0] = -ksyl / 2
         half_cor = np.dot(np.dot(drift, quad), drift)
 
     m0c = cumul_mat[cor]
-    if meth.lower().startswith('mid'):
+    if meth.lower().startswith("mid"):
         m0c = np.linalg.solve(half_cor, m0c)
     mat = np.linalg.solve(m0c.T, cumul_mat[bpms].transpose((0, 2, 1)))
     mat = mat.transpose(0, 2, 1)
@@ -302,8 +329,17 @@ def _get_respmat_line(
 class FindSeptQuad(SimulAnneal):
     """."""
 
-    def __init__(self, tb_model, bo_model, corr_names, elems,
-                 respmat, nturns=5, save=False, in_sept=True):
+    def __init__(
+        self,
+        tb_model,
+        bo_model,
+        corr_names,
+        elems,
+        respmat,
+        nturns=5,
+        save=False,
+        in_sept=True,
+    ):
         """."""
         super().__init__(save=save)
         self.tb_model = tb_model
@@ -322,13 +358,15 @@ class FindSeptQuad(SimulAnneal):
         """."""
         if self.in_sept:
             sept_idx = pyaccel.lattice.find_indices(
-                self.tb_model, 'fam_name', 'InjSept')
+                self.tb_model, "fam_name", "InjSept"
+            )
         else:
-            sept_idx = self.elems['TB-04:MA-CV-2'].model_indices
+            sept_idx = self.elems["TB-04:MA-CV-2"].model_indices
         k, ks = self._position
-        pyaccel.lattice.set_attribute(self.tb_model, 'K', sept_idx, k)
-        pyaccel.lattice.set_attribute(self.tb_model, 'Ks', sept_idx, ks)
+        pyaccel.lattice.set_attribute(self.tb_model, "K", sept_idx, k)
+        pyaccel.lattice.set_attribute(self.tb_model, "Ks", sept_idx, ks)
         respmat = calc_model_respmatTBBO(
-            self.tb_model, self.bo_model, self.corr_names, self.elems)
+            self.tb_model, self.bo_model, self.corr_names, self.elems
+        )
         respmat -= self.respmat
-        return np.sqrt(np.mean(respmat*respmat))
+        return np.sqrt(np.mean(respmat * respmat))
